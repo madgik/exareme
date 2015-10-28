e75121a7d4414a543ee3279334243d654eb2d5d4
alex <alpap@di.uoa.gr>
2015-10-28 10:19:25 +0200

    clean target/ + .gitignore added.

--- exareme-master/target/test-classes/madgik/exareme/master/app/cluster/load_emp_template.sql
+++
@@ -1,7 +0,0 @@
-distributed create table %1$s to %2$s on eid as
-select
-  cast(c1 as int) as eid,
-  cast(c2 as text) as ename,
-  cast(c3 as int) as age,
-  cast(c4 as float) as salary
-from file('%3$s');
