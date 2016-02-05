494653242fd1683d2a8c94037d22dade94ea0469
alex <alpap@di.uoa.gr>
2016-02-05 08:43:08 +0200

    exareme : exareme-test added (for algorithms)

--- exareme-tools/algorithms/STATISTICS/global.template.sql
+++ exareme-tools/algorithms/STATISTICS/global.template.sql
@@ -1,13 +1,19 @@
 requirevars 'input_global_tbl' ;
 
-select  colname,
-        FARITH(NA) as counts,
-        min(minval) as mivalue,
-        max(maxval) as maxvalue,
-        FARITH(S1A) as sumvalue,
-        FARITH('/',S1A,NA) as avgvalue,
-        SQROOT( FARITH('/', '-', '*', NA, S2A, '*', S1A, S1A, '*', NA, '-', NA, 1)) as stdvalue
-from (  select colname, FSUM(S2) as S2A, FSUM(S1) as S1A, SUM(N) as NA
-        from %{input_global_tbl}
-        group by colname
-);
+-- select  colname,
+--         FARITH(NA) as counts,
+--         minvalue,
+--         maxvalue,
+--         FARITH(S1A) as sumvalue,
+--         FARITH('/',S1A,NA) as avgvalue,
+--         SQROOT( FARITH('/', '-', '*', NA, S2A, '*', S1A, S1A, '*', NA, '-', NA, 1)) as stdvalue
+-- from (  select colname,
+--                FSUM(S2) as S2A,
+--                FSUM(S1) as S1A,
+--                SUM(N) as NA,
+--                min(minval) as minvalue,
+--                max(maxval) as maxvalue
+--         from %{input_global_tbl}
+--         group by colname
+-- );
+select * from %{input_global_tbl};
