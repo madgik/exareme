e0b0ab7519f47adf79de26cc92436af05b1b14b0
Yannis Chronis <johnchronis@gmail.com>
2017-01-03 13:28:04 +0200

    docker properties added

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,26 @@
 echo `(hostname --ip-address)` > /root/exareme/etc/exareme/master
 echo "" > /root/exareme/etc/exareme/workers
 
+rm /root/exareme/etc/exareme/gateway.properties
+
+mv /root/exareme/etc/exareme/gateway_docker.properties /root/exareme/etc/exareme/gateway.properties
+
+rm /root/exareme/lib/algorithms-dev/properties.json
+
+mv /root/exareme/lib/algorithms-dev/properties_docker.json /root/exareme/lib/algorithms-dev/properties.json
 
 mkdir -p  /tmp/demo/db/
 #/bin/bash
-./bin/exareme-admin.sh --start --local && tail -f /tmp/exareme/var/log/exareme-*.log
+
+
+./bin/exareme-admin.sh --start --local
+
+if [[ -e "/tmp/exareme/var/log/exareme-*.log" ]]; then
+
+    tail -f /tmp/exareme/var/log/exareme-*.log
+else
+    sleep 2;
+    tail -f /tmp/exareme/var/log/exareme-*.log
+fi
+
+
