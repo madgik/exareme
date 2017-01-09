69e8eabc53836a49369c2a0d9d28d546fb7c51c1
Chris Aslanoglou <chris.aslanoglou@gmail.com>
2017-01-09 16:14:18 +0200

    Add IterationsStateManagerImpl
    - Implements IterationsStateManager interface using 2 maps and especially
      ConcurrentMaps, so as to support concurrent additions/removals/access
      during multiple iterative algorithms execution.
    - It is implemented as a singleton since there's no need for having
      multiple IterationsStateManagers for an Exareme instance.

---
+++ exareme-master/src/main/java/madgik/exareme/master/engine/iterations/state/IterationsStateManagerImpl.java
@@ -0,0 +1,131 @@
+package madgik.exareme.master.engine.iterations.state;
+
+import org.apache.log4j.Logger;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import madgik.exareme.common.app.engine.AdpDBQueryID;
+import madgik.exareme.master.engine.iterations.exceptions.IterationsFatalException;
+
+/**
+ * @author Christos Aslanoglou <br> caslanoglou@di.uoa.gr <br> University of Athens / Department of
+ *         Informatics and Telecommunications.
+ */
+public class IterationsStateManagerImpl implements IterationsStateManager {
+    private static final Logger log = Logger.getLogger(IterationsStateManagerImpl.class);
+
+    // Mapping from algorithmKey -> IterativeAlgorithmState.
+    private ConcurrentHashMap<String, IterativeAlgorithmState> iterativeAlgorithmMapping;
+
+    // Mapping from QueryID -> AlgorithmKey.
+    // To be used from listeners, so as to access the algorithm's state.
+    private ConcurrentHashMap<AdpDBQueryID, String> queryIdToAlgorithmKeyMapping;
+
+    // Singleton related ------------------------------------------------------------------------
+    private IterationsStateManagerImpl() {
+        iterativeAlgorithmMapping = new ConcurrentHashMap<>();
+        queryIdToAlgorithmKeyMapping = new ConcurrentHashMap<>();
+    }
+
+    private static IterationsStateManagerImpl instance = new IterationsStateManagerImpl();
+
+    public static IterationsStateManagerImpl getInstance() {
+        return instance;
+    }
+    // ------------------------------------------------------------------------------------------
+
+    // IterationsStateManager API ---------------------------------------------------------------
+    // Related to algorithmKey -> IterativeAlgorithmState mapping -------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void submitIterativeAlgorithm(String algorithmKey, IterativeAlgorithmState iterativeAlgorithmState) {
+        if (algorithmKey == null || iterativeAlgorithmState == null) {
+            String msg = algorithmKey == null ? "algorithmKey " : "";
+            msg += iterativeAlgorithmState == null ?
+                    (!msg.isEmpty() ? " or iterativeAlgorithmState " : "iterativeAlgorithmState ")
+                    : "";
+            msg += "cannot be null.";
+            throw new IllegalArgumentException(msg);
+        }
+
+        IterativeAlgorithmState dummyIterAlgorithmState = iterativeAlgorithmMapping.get(algorithmKey);
+        if (dummyIterAlgorithmState != null)
+            throw new IterationsFatalException("An IterativeAlgorithmState entry ("
+                    + dummyIterAlgorithmState.toString() + ") tied to the " +
+                    "specified algorithmKey (\"" + algorithmKey + "\" already exists.");
+
+        iterativeAlgorithmMapping.put(algorithmKey, iterativeAlgorithmState);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public IterativeAlgorithmState removeIterativeAlgorithm(String algorithmKey) {
+        if (algorithmKey == null)
+            throw new IllegalArgumentException("algorithmKey cannot be null.");
+
+        return iterativeAlgorithmMapping.remove(algorithmKey);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public IterativeAlgorithmState getIterativeAlgorithm(String algorithmKey) {
+        if (algorithmKey == null)
+            throw new IllegalArgumentException("algorithmKey cannot be null.");
+
+        return iterativeAlgorithmMapping.get(algorithmKey);
+    }
+
+    // Related to queryID -> algorithmKey mapping -----------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void submitQueryForIterativeAlgorithm(String algorithmKey, AdpDBQueryID queryID) {
+        if (algorithmKey == null || queryID == null) {
+            String msg = algorithmKey == null ? "algorithmKey " : "";
+            msg += queryID == null ?
+                    (!msg.isEmpty() ? " or queryID " : "queryID ")
+                    : "";
+            msg += "cannot be null.";
+            throw new IllegalArgumentException(msg);
+        }
+        if (queryIdToAlgorithmKeyMapping.get(queryID) != null)
+            throw new IterationsFatalException("An algorithmKey (\"" + algorithmKey + "\") entry " +
+                    "tied to the specified queryID (\"" + queryID + "\") already exists.");
+
+        queryIdToAlgorithmKeyMapping.put(queryID, algorithmKey);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeQueryOfIterativeAlgorithm(AdpDBQueryID queryID) {
+        if (queryID == null)
+            throw new IllegalArgumentException("queryID cannot be null.");
+
+        queryIdToAlgorithmKeyMapping.remove(queryID);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public IterativeAlgorithmState getIterativeAlgorithm(AdpDBQueryID queryID) {
+        if (queryID == null)
+            throw new IllegalArgumentException("queryID cannot be null.");
+
+        String algorithmKey = queryIdToAlgorithmKeyMapping.get(queryID);
+        if (algorithmKey == null)
+            return null;
+        return iterativeAlgorithmMapping.get(algorithmKey);
+    }
+}
