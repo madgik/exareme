3bd0f9d70633790e5717f4ff661e26192c2c15fa
Chris Aslanoglou <chris.aslanoglou@gmail.com>
2017-01-10 19:20:55 +0200

    Add dynamic DFL generation (support context between iter. phases)
    Adds context support between iterative phases by saving each time the
    latest phase outputTblName and using it as input for the currently
    generated DFL script.

--- exareme-master/src/main/java/madgik/exareme/master/engine/iterations/state/IterativeAlgorithmState.java
+++ exareme-master/src/main/java/madgik/exareme/master/engine/iterations/state/IterativeAlgorithmState.java
@@ -1,15 +1,21 @@
 package madgik.exareme.master.engine.iterations.state;
 
+import org.apache.commons.lang3.text.StrSubstitutor;
 import org.apache.log4j.Logger;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
+import madgik.exareme.master.engine.iterations.exceptions.IterationsFatalException;
+import madgik.exareme.master.engine.iterations.handler.IterationsHandlerConstants;
+import madgik.exareme.master.engine.iterations.handler.IterationsHandlerDFLUtils;
 import madgik.exareme.master.engine.iterations.state.exceptions.IterationsStateFatalException;
 import madgik.exareme.master.queryProcessor.composer.AlgorithmsProperties;
 
 import static madgik.exareme.master.engine.iterations.handler.IterationsHandlerConstants.iterationsPropertyConditionQueryProvided;
 import static madgik.exareme.master.engine.iterations.handler.IterationsHandlerConstants.iterationsPropertyMaximumNumber;
+import static madgik.exareme.master.engine.iterations.handler.IterationsHandlerConstants.previousPhaseOutputTblVariableName;
 
 /**
  * @author Christos Aslanoglou <br> caslanoglou@di.uoa.gr <br> University of Athens / Department of
@@ -39,8 +45,11 @@ public class IterativeAlgorithmState {
     private AlgorithmsProperties.AlgorithmProperties algorithmProperties;
     private HashMap<String, String> algorithmPropertiesMap;
     private String[] dflScripts;
-    private String workingDirectory;
-
+    /**
+     * Variable name and key of the {@code dflVariablesMap} to be used for DFL scripts variable
+     * StrSubstitution.
+     */
+    final private String stepPhaseOutputTblVariableName;
     // Iterations control-plane related fields [properties.json] --------------------------------
     private Boolean conditionQueryProvided;
     private Long maxIterationsNumber;
@@ -50,6 +59,15 @@ public class IterativeAlgorithmState {
     // started.
     private IterativeAlgorithmPhasesModel currentExecutionPhase;
     private Long currentIterationsNumber;
+    /**
+     * Used in conjunction with StrSubstitutor to replace variables in DFL scripts.
+     *
+     * <p> Mapping contains: <br>
+     *  1. {@code IterationsHandlerConstants.previousPhaseOutputTblVariableName -> latestPhaseOutputTblName}<br>
+     *  2. {@code stepPhaseOutputTblVariableName -> currentStepPhaseOutputTblName}<br>
+     */
+    private Map<String, String> dflVariablesMap;
+
     // The lock will be used to ensure no data-races occur after the pre-algorithm-execution phase,
     // for the [STATE] fields above.
     private final ReentrantLock lock = new ReentrantLock();
@@ -62,8 +80,17 @@ public class IterativeAlgorithmState {
         algorithmPropertiesMap =
                 AlgorithmsProperties.AlgorithmProperties.toHashMap(algorithmProperties);
         setUpPropertyFields();
+
         // State related fields initialization
         currentExecutionPhase = null;
+        stepPhaseOutputTblVariableName = IterationsHandlerDFLUtils.getStepPhaseOutputTblVariableName(algorithmKey);
+
+        dflVariablesMap = new HashMap<>();
+        // Initialize with init phase's output tbl name (it's always the first lookup)
+        dflVariablesMap.put(
+                IterationsHandlerConstants.previousPhaseOutputTblVariableName,
+                IterationsHandlerDFLUtils.getInitPhaseOutputTblName(algorithmKey));
+        dflVariablesMap.put(stepPhaseOutputTblVariableName, null);
     }
 
     // IterativeAlgorithmState - Set property fields --------------------------------------------
@@ -130,26 +157,56 @@ public class IterativeAlgorithmState {
         return conditionQueryProvided;
     }
 
-    public void setConditionQueryProvided(Boolean conditionQueryProvided) {
-        this.conditionQueryProvided = conditionQueryProvided;
-    }
-
-    public Long getMaxIterationsNumber() {
-        return maxIterationsNumber;
-    }
-
     public void setDflScripts(String[] dflScripts) {
         this.dflScripts = dflScripts;
     }
 
-    /**
-     * Returns the DFL script of a specific phase ({@code phase}).
-     */
-    public String getDFLScript(IterativeAlgorithmPhasesModel phase) {
-        return dflScripts[phase.ordinal()];
-    }
     // Execution phase fields ==========================
     // i.e. These methods must be called with the lock acquired.
+
+    /**
+     * Returns the DFL script of the given iterative phase.
+     *
+     * <p> For step and finalize phases it runs an strSubstitution for replacing the previous
+     * output phase placeholder accordingly.
+     * <b>Completes two tasks</b>:<br>
+     *     1. generates DFL for requested phase, <br>
+     *     2. sets the {@code latestPhaseOutputTblName} in the {@code dflVariablesMap}.
+     */
+    public String getDFLScript(IterativeAlgorithmPhasesModel phase) {
+        ensureAcquiredLock();
+        String dflScript;
+        // DFL for init and termination condition are already ("statically") generated, simply return
+        // For other phases, substitute variables and return generated String
+        switch (phase) {
+            case init:
+                dflScript = dflScripts[phase.ordinal()];
+                break;
+            case step:
+                // Retrieve previousPhase outputTbl name & generate currentStep's outputTbl name
+                String previousPhaseOutputTbl = dflVariablesMap.get(previousPhaseOutputTblVariableName);
+                String currentStepOutputTblName = generateStepPhaseCurrentOutputTbl();
+                dflVariablesMap.put(IterationsHandlerConstants.previousPhaseOutputTblVariableName,
+                        previousPhaseOutputTbl);
+                dflVariablesMap.put(stepPhaseOutputTblVariableName, currentStepOutputTblName);
+
+                dflScript = StrSubstitutor.replace(dflScripts[phase.ordinal()], dflVariablesMap);
+
+                // Update previousPhaseOutputTbl name with currentStep's output tbl name
+                dflVariablesMap.put(IterationsHandlerConstants.previousPhaseOutputTblVariableName,
+                        currentStepOutputTblName);
+                break;
+            case termination_condition:
+                return dflScripts[phase.ordinal()];
+            case finalize:
+                dflScript = StrSubstitutor.replace(dflScripts[phase.ordinal()], dflVariablesMap);
+                break;
+                default:
+                    throw new IterationsFatalException("IterativePhase: \"" + phase.name()
+                            + "\" is not supported yet");
+        }
+        return dflScript;
+    }
 
     /**
      * Retrieves the current iterations number.
@@ -165,7 +222,7 @@ public class IterativeAlgorithmState {
      */
     public void incrementIterationsNumber() {
         ensureAcquiredLock();
-        this.currentIterationsNumber = currentIterationsNumber;
+        this.currentIterationsNumber++;
     }
 
     /**
@@ -232,5 +289,15 @@ public class IterativeAlgorithmState {
         return "IterativeStateAlgorithm{\"" +
                 algorithmProperties.getName() + "\"} [" +
                 currentStateMsg + "]";
+    }
+
+    /**
+     * Generates the step phase's current outputTbl name in the format
+     * {@code stepPhaseOutputTblVariableName_currentIterationNumber}.
+     */
+    private String generateStepPhaseCurrentOutputTbl() {
+        if (currentIterationsNumber == null)
+            currentIterationsNumber = 1L;
+        return stepPhaseOutputTblVariableName + "_" + currentIterationsNumber;
     }
 }
