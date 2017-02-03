e587ea92a7975c3c519f3db7871087e2a2113ff6
Yannis Chronis <johnchronis@gmail.com>
2017-02-03 20:15:06 +0200

    playing with docker mip local confs

---
+++ exareme-docker/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+cp ../exareme-utils/src/main/resources/gateway.properties .
+sed -i '/composer.repository.path=/c\composer.repository.path=/root/mip-algorithms/' gateway.properties
+sed -i '/static.path/c\static.path=/root/exareme/static/' gateway.properties
+
+
+
+docker build -t exareme-mip-local .
---
+++ exareme-docker/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -i -t --rm -p 9090:9090  --name exareme-mip-local exareme-mip-local
