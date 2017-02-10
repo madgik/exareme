0478636f6eeb55a21565cfc58e52152fbcb6a8d3
Yannis Chronis <johnchronis@gmail.com>
2017-02-10 02:50:52 +0200

    cleanup of docker#1

--- exareme-docker/build.sh
+++ exareme-docker/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 
-#cp ../exareme-utils/src/main/resources/gateway.properties .
-#sed -i '/composer.repository.path=/c\composer.repository.path=/root/mip-algorithms/' gateway.properties
-#sed -i '/static.path/c\static.path=/root/exareme/static/' gateway.properties
-#
 
-
-docker build -t hbpmip/exaremelocal .
+docker build -t exaremelocal .
