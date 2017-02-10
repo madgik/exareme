0f7eb4e8fc0d96e2f622fe1560abf513fd341e10
Yannis Chronis <johnchronis@gmail.com>
2017-02-10 02:13:29 +0200

    compose for exalocal works

--- exareme-docker/bootstrap.sh
+++ exareme-docker/bootstrap.sh
@@ -16,21 +16,21 @@ echo "" > /root/exareme/etc/exareme/workers
 
 sed -i "/<raw_username>/c{ \"name\" : \"username\", \"desc\" : \"\", \"value\":\"`echo $RAWUSERNAME`\" }," /root/mip-algorithms/properties.json
 sed -i "/<raw_password>/c{ \"name\" : \"password\", \"desc\" : \"\", \"value\":\"`echo $RAWPASSWORD`\" }," /root/mip-algorithms/properties.json
-sed -i "/<raw_host>/c{ \"name\" : \"password\", \"desc\" : \"\", \"value\":\"`echo $RAWHOST`\" }," /root/mip-algorithms/properties.json
-sed -i "/<raw_port>/c{ \"name\" : \"password\", \"desc\" : \"\", \"value\":\"`echo $RAWPORT`\" }" /root/mip-algorithms/properties.json
+sed -i "/<raw_host>/c{ \"name\" : \"host\", \"desc\" : \"\", \"value\":\"`echo $RAWHOST`\" }," /root/mip-algorithms/properties.json
+sed -i "/<raw_port>/c{ \"name\" : \"port\", \"desc\" : \"\", \"value\":\"`echo $RAWPORT`\" }" /root/mip-algorithms/properties.json
 
 
 mkdir -p  /tmp/demo/db/
 
-/bin/bash
+#/bin/bash
 
-#./bin/exareme-admin.sh --start --local
-#
-#if [[ -e "/tmp/exareme/var/log/exareme-*.log" ]]; then
-#
-#    tail -f /tmp/exareme/var/log/exareme-*.log
-#else
-#    sleep 2;
-#    tail -f /tmp/exareme/var/log/exareme-*.log
-#fi
-#
+./bin/exareme-admin.sh --start --local
+
+if [[ -e "/tmp/exareme/var/log/exareme-*.log" ]]; then
+
+    tail -f /tmp/exareme/var/log/exareme-*.log
+else
+    sleep 2;
+    tail -f /tmp/exareme/var/log/exareme-*.log
+fi
+
