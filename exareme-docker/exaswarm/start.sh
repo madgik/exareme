ed081fd675c70b34f52f7fa9e58be26a7280018e
Vaggelis Nikolopoulos <vgnikolop@di.uoa.gr>
2017-03-21 19:03:50 +0200

    Adding simple start-stop scripts for exaswarm

--- exareme-docker/exaswarm/start.sh
+++ exareme-docker/exaswarm/start.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
-
-
-docker run -i -t --rm \
--e MASTER_FLAG='master' \
--e RAWUSERNAME="federation" \
--e RAWPASSWORD="federation" \
--e RAWHOST="raw-ui" \
--e RAWPORT="5555" \
--e RAWENDPOINT="query-start" \
--e RAWRESULTS="all" \
--e RAWDATAKEY="output" \
--p 9090:9090  --name exaremelocal exaremelocal
+./startworkers.sh
+./startmaster.sh
---
+++ exareme-docker/exaswarm/stop.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker service rm exareme-worker
