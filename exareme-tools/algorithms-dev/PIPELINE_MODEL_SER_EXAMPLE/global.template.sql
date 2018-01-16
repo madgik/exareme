37fdf6d42a8431d4cd88ee78509c6371287eedf0
vnikolopoulos <vgnikolop@di.uoa.gr>
2018-01-16 17:46:03 +0200

    pipeline model update

--- exareme-tools/algorithms-dev/PIPELINE_MODEL_SER_EXAMPLE/global.template.sql
+++ exareme-tools/algorithms-dev/PIPELINE_MODEL_SER_EXAMPLE/global.template.sql
@@ -1,3 +1,6 @@
 requirevars 'prv_output_local_tbl';
 
-select jdict("res", count(*)) from %{prv_output_local_tbl};
+select writebinary('/root/exareme/static/exa-view/results/model.ser', bin)
+from  %{prv_output_local_tbl};
+
+select jdict("Download Model from: ","http://localhost:9090/exa-view/results/model.ser");
--- exareme-tools/algorithms-dev/PIPELINE_MODEL_SER_EXAMPLE/localupdate.template.sql
+++ exareme-tools/algorithms-dev/PIPELINE_MODEL_SER_EXAMPLE/localupdate.template.sql
@@ -1,9 +1,9 @@
 requirevars 'prv_output_local_tbl';
 
 
-select writebinary('tree.ser.new', bin) from  %{prv_output_local_tbl};
+select writebinary('model.ser.prev', bin) from  %{prv_output_local_tbl};
 
-select execprogram(null, 'java', '-jar', 'Serialization.jar', 'bicycles.arff', '13-15');
+select execprogram(null, 'java', '-jar', 'Serialization.jar', 'bicycles.arff', '13-15', 'model.ser.prev');
 
-select execprogram(null,'cat','tree.ser') as bin ;
-
+select bin from (unindexed select bin, execprogram(null,'rm',c2)  from
+(unindexed select c2, execprogram(null,'cat',c2) as bin from dirfiles(.) where c2 like "tree%ser"));
