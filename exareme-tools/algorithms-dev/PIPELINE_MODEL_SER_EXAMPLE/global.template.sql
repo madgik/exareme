86522c7f89ec9b4266d99cc84f3a7b8e7d3442db
vnikolopoulos <vgnikolop@di.uoa.gr>
2018-01-17 19:15:02 +0200

    pipeline example with raw

--- exareme-tools/algorithms-dev/PIPELINE_MODEL_SER_EXAMPLE/global.template.sql
+++ exareme-tools/algorithms-dev/PIPELINE_MODEL_SER_EXAMPLE/global.template.sql
@@ -3,4 +3,5 @@ requirevars 'prv_output_local_tbl';
 select writebinary('/root/exareme/static/exa-view/results/model.ser', bin)
 from  %{prv_output_local_tbl};
 
-select jdict("Download Model from: ","http://prozac.madgik.di.uoa.gr:9090/exa-view/results/model.ser");
+select jdict("Download Model from: ", "http://prozac.madgik.di.uoa.gr:9090/exa-view/results/model.ser");
+
