6b01e3b653c8d61f1d7914f5c299758e6400ec7b
sofiakarb <karvoun@di.uoa.gr>
2019-03-14 17:03:02 +0200

    Delete unwanted folder for algorithms-dev. Delete files exareme-env.sh,exareme-admin.sh. Now under Exareme-docker folder

--- exareme-master/src/main/resources/exareme-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export EXAREME_USER="$USER" # TODO JC "root"
-export EXAREME_JAVA="$(which java)"
-export EXAREME_JAVA_OPTS="-Xms512M -Xmx2046M"
-export EXAREME_PYTHON="$(which python)"
-export EXAREME_MADIS="$EXAREME_HOME/lib/madis/src/mterm.py"
-export MADIS_PATH=$EXAREME_MADIS
-/home/johnchronis/code/exareme/exareme-master/src/main/resources/exareme-env.sh
--- exareme-tools/algorithms-dev/WP_PFA_PIPELINE/local.template.sql
+++
@@ -1,6 +0,0 @@
-requirevars 'defaultDB' 'input_local_tbl' 'local_pfa';
-
-create temporary table inputlocaltbl as
-select * from %{input_local_tbl};
-
-select * from (madtitus 'pfa:%{local_pfa}' select * from inputlocaltbl);
