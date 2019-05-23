1fbd34f1749bc024ba49c1f6e48af1862412a8b5
sofiakarb <karvoun@di.uoa.gr>
2019-05-23 14:24:30 +0300

    Small fixes

--- Exareme-Docker/src/mip-algorithms/LINEAR_REGRESSION/3/local.template.sql
+++ Exareme-Docker/src/mip-algorithms/LINEAR_REGRESSION/3/local.template.sql
@@ -4,6 +4,7 @@ attach database '%{defaultDB}' as defaultDB;
 --var 'prv_output_global_tbl' 'defaultDB.globalcoefficientsandstatistics'; -- statistics & coefficients
 
 --E1. Compute residuals y-ypredictive = Y-sum(X(i)*estimate(i)) (Local Layer)
+
 var 'a' from select tabletojson(attr1,estimate,"attr1,estimate") from %{prv_output_global_tbl} where tablename ="coefficients";
 
 drop table if exists defaultDB.residuals;
@@ -23,9 +24,9 @@ var 'sume' from select sum(val) from defaultDB.residuals;
 var 'sse' from select sum(val*val) from defaultDB.residuals;
 var 'sst' from select sum( (%{y}-%{grandmean})*(%{y}-%{grandmean})) from defaultdb.localinputtblflat;
 
-
 drop table if exists defaultDB.localLRresults;
-create table defaultDB.localLRresults as
+create table defaultDB.localLRresults (rows int,cols int,mine real, maxe real, sume real, sst real, sse real);
+insert into  defaultDB.localLRresults
 select  '%{rows}' as rows, '%{cols}' as cols,
         '%{mine}' as mine, '%{maxe}' as maxe,'%{sume}' as sume,
         '%{sst}' as sst,'%{sse}' as sse ;
