d59d3cc35717163e6076fdb3a71b2189eb546cc7
ThanKarab <tkarabatsis@hotmail.com>
2019-06-26 15:22:42 +0300

    Renamed variablesMetadata.json to CDEsMetadata.json

--- Exareme-Docker/src/exareme/exareme-master/src/main/java/madgik/exareme/master/queryProcessor/composer/CDEsMetadata.java
+++ Exareme-Docker/src/exareme/exareme-master/src/main/java/madgik/exareme/master/queryProcessor/composer/CDEsMetadata.java
@@ -3,31 +3,31 @@ package madgik.exareme.master.queryProcessor.composer;
 import java.sql.*;
 import java.util.HashMap;
 
-import madgik.exareme.master.queryProcessor.composer.Exceptions.VariablesMetadataException;
+import madgik.exareme.master.queryProcessor.composer.Exceptions.CDEsMetadataException;
 import org.apache.log4j.Logger;
 
 /**
  * Loads the variables' metadata from the database.
  */
-public class VariablesMetadata {
+public class CDEsMetadata {
     private static Logger log = Logger.getLogger(Composer.class);
-    private static VariablesMetadata instance = null;
-    private HashMap<String, VariableProperties> variablesMetadata;
+    private static CDEsMetadata instance = null;
+    private HashMap<String, CDEProperties> CDEsMetadata;
 
-    private class VariableProperties {
+    private class CDEProperties {
         String sql_type;
         String categorical;
         String enumerations;
         int enumerationsCount;
 
-        private VariableProperties(String sql_type, int categorical, String enumerations) throws VariablesMetadataException {
+        private CDEProperties(String sql_type, int categorical, String enumerations) throws CDEsMetadataException {
             this.sql_type = sql_type;
             if (categorical == 1)
                 this.categorical = "true";
             else if (categorical == 0)
                 this.categorical = "false";
             else
-                throw new VariablesMetadataException("Categorical values can be 0 or 1 in the metadata.");
+                throw new CDEsMetadataException("Categorical values can be 0 or 1 in the metadata.");
             this.enumerations = enumerations;
             enumerationsCount = enumerations.split(",").length;
         }
@@ -45,17 +45,17 @@ public class VariablesMetadata {
         }
     }
 
-    public static VariablesMetadata getInstance() throws VariablesMetadataException {
+    public static CDEsMetadata getInstance() throws CDEsMetadataException {
         if (instance == null) {
-            instance = new VariablesMetadata();
+            instance = new CDEsMetadata();
         }
         return instance;
     }
 
-    private VariablesMetadata() throws VariablesMetadataException {
+    private CDEsMetadata() throws CDEsMetadataException {
         String databasePath = ComposerConstants.getDatasetDBDirectory();
         String metadataTablename = ComposerConstants.getDBMetadataTablename();
-        variablesMetadata = new HashMap<>();
+        CDEsMetadata = new HashMap<>();
 
         try {
 
@@ -75,30 +75,30 @@ public class VariablesMetadata {
                 if (enumerations == null)
                     enumerations = "";
 
-                variablesMetadata.put(code, new VariableProperties(sql_type, categorical, enumerations));
+                CDEsMetadata.put(code, new CDEProperties(sql_type, categorical, enumerations));
             }
             rs.close();
             stmt.close();
             c.close();
         } catch (Exception e) {
-            throw new VariablesMetadataException("Could not load variables metadata. Error: " + e.getMessage());
+            throw new CDEsMetadataException("Could not load variables metadata. Error: " + e.getMessage());
         }
         log.debug("Metadata loaded successfully");
     }
 
     public Boolean columnExists(String code) {
-        return variablesMetadata.containsKey(code);
+        return CDEsMetadata.containsKey(code);
     }
 
     public String getColumnValuesSQLType(String code) {
-        return variablesMetadata.get(code).getSql_type();
+        return CDEsMetadata.get(code).getSql_type();
     }
 
     public String getColumnValuesIsCategorical(String code) {
-        return variablesMetadata.get(code).getCategorical();
+        return CDEsMetadata.get(code).getCategorical();
     }
 
     public int getColumnValuesNumOfEnumerations(String code) {
-        return variablesMetadata.get(code).getEnumerationsCount();
+        return CDEsMetadata.get(code).getEnumerationsCount();
     }
 }
---
+++ Exareme-Docker/src/exareme/exareme-master/src/main/java/madgik/exareme/master/queryProcessor/composer/Exceptions/CDEsMetadataException.java
@@ -0,0 +1,7 @@
+package madgik.exareme.master.queryProcessor.composer.Exceptions;
+
+public class CDEsMetadataException extends Exception {
+    public CDEsMetadataException(String message) {
+        super(message);
+    }
+}
