0206921564effeaf217b6ccc4d0f197c9460a35d
sofiakarb <karvoun@di.uoa.gr>
2019-07-18 18:22:22 +0300

    During the execution of an algorithm, check that the connection can be established with each Exareme node. If that is not the case, delete IP from Exareme's registry and infos regarding that node from Consul[Key-Value store]. Still Todo show proper error message to user

--- Exareme-Docker/src/exareme/exareme-worker/src/main/java/madgik/exareme/worker/art/remote/RmiObjectProxy.java
+++ Exareme-Docker/src/exareme/exareme-worker/src/main/java/madgik/exareme/worker/art/remote/RmiObjectProxy.java
@@ -3,12 +3,27 @@
  */
 package madgik.exareme.worker.art.remote;
 
+import com.google.gson.Gson;
 import madgik.exareme.common.art.entity.EntityName;
+import madgik.exareme.worker.art.container.ContainerProxy;
+import madgik.exareme.worker.art.registry.ArtRegistryLocator;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
 import java.rmi.AccessException;
 import java.rmi.RemoteException;
+import java.rmi.ServerException;
 import java.rmi.registry.Registry;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * University of Athens /
@@ -62,10 +77,156 @@ public abstract class RmiObjectProxy<T> implements ObjectProxy<T> {
 
     @Override
     public T getRemoteObject() throws RemoteException {
+        String name = null;
+        Iterator<Map.Entry<String, String>> entries;
+
         if (isConnected == false) {
-            connect();
+            try {
+                connect();      //try to connect to remote object. If the connection is failing, maybe java is not running
+            } catch (RemoteException exception) {
+
+                //Get the Exareme's node name that is not responding
+                HashMap<String,String> names = null;
+                try {
+                    names = getNamesOfActiveNodes();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if(names != null) {
+                    entries = names.entrySet().iterator();
+                    while (entries.hasNext()) {
+                        Map.Entry<String, String> entry = entries.next();
+                        if (Objects.equals(entry.getKey(), regEntityName.getIP())) {
+                            name = entry.getValue();
+                            break;
+                        }
+                    }
+                    //Search if the Exareme's node IP exist in Exareme's registry
+                    for (ContainerProxy containerProxy : ArtRegistryLocator.getArtRegistryProxy().getContainers()) {
+                        log.debug("Container: " + containerProxy.getEntityName().getIP() + " : " +
+                                containerProxy.getEntityName().getName());
+                        if (containerProxy.getEntityName().getIP().equals(regEntityName.getIP())) {
+                            //If exists, remove it from Exareme's registry
+                            ArtRegistryLocator.getArtRegistryProxy().removeContainer(containerProxy.getEntityName());
+                            log.debug("Worker node:[" + name + "," + regEntityName.getIP() + "]" + " removed successfully from Exareme's registry");
+
+                            //If exist in Consul[Key-Value store], delete infos regarding that Exareme node from there
+                            if (name != null) {
+                                try {
+                                    deleteFromConsul(System.getenv("DATASETS") + "/" + name);
+                                    deleteFromConsul(System.getenv("EXAREME_ACTIVE_WORKERS_PATH") + "/" + name);
+                                    log.debug("Worker node:[" + name + "," + regEntityName.getIP() + "]" + "removed from Consul key-value store");
+                                } catch (IOException E) {
+                                    throw new RemoteException("Can not contact Consul Key value Store");
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+                throw new RemoteException("There was an error with some of the workers. The user need to be informed"); //TODO catch the error
+            }
         }
         return remoteObject;
+    }
+
+    private HashMap <String,String> getNamesOfActiveNodes() throws IOException {
+        Gson gson = new Gson();
+        HashMap <String,String> map = new HashMap<>();
+        String masterKey = searchConsul(System.getenv("EXAREME_MASTER_PATH")+"/?keys");
+        String[] masterKeysArray = gson.fromJson(masterKey, String[].class);
+
+        String masterName = masterKeysArray[0].replace(System.getenv("EXAREME_MASTER_PATH")+"/", "");
+        String masterIP = searchConsul(System.getenv("EXAREME_MASTER_PATH")+"/"+masterName+"?raw");
+        map.put(masterIP,masterName);
+
+        String workersKey = searchConsul(System.getenv("EXAREME_ACTIVE_WORKERS_PATH")+"/?keys");
+        if (workersKey == null)   //No workers running
+            return map;             //return master only
+        String[] workerKeysArray = gson.fromJson(workersKey, String[].class);
+        for(String worker: workerKeysArray){
+            String workerName = worker.replace(System.getenv("EXAREME_ACTIVE_WORKERS_PATH")+"/", "");
+            String workerIP = searchConsul(System.getenv("EXAREME_ACTIVE_WORKERS_PATH")+"/"+workerName+"?raw");
+            map.put(workerIP,workerName);
+        }
+        return map;
+    }
+
+    private String searchConsul(String query) throws IOException {
+        String result = null;
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        String consulURL = System.getenv("CONSULURL");
+        if (consulURL == null) throw new IOException("Consul url not set");
+        if (!consulURL.startsWith("http://")) {
+            consulURL = "http://" + consulURL;
+        }
+        try {
+            HttpGet httpGet;
+            httpGet = new HttpGet(consulURL + "/v1/kv/" + query);
+            log.debug("Running: " + httpGet.getURI());
+            CloseableHttpResponse response = null;
+            if (httpGet.toString().contains(System.getenv("EXAREME_MASTER_PATH")+"/") || httpGet.toString().contains(System.getenv("DATASETS")+"/")) {    //if we can not contact : http://exareme-keystore:8500/v1/kv/master* or http://exareme-keystore:8500/v1/kv/datasets*
+                try {   //then throw exception
+                    response = httpclient.execute(httpGet);
+                    if (response.getStatusLine().getStatusCode() != 200) {
+                        throw new ServerException("Cannot contact consul", new Exception(EntityUtils.toString(response.getEntity())));
+                    } else {
+                        result = EntityUtils.toString(response.getEntity());
+                    }
+                } finally {
+                    response.close();
+                }
+            }
+            if (httpGet.toString().contains(System.getenv("EXAREME_ACTIVE_WORKERS_PATH")+"/")) {    //if we can not contact : http://exareme-keystore:8500/v1/kv/active_workers*
+                try {   //then maybe there are no workers running
+                    response = httpclient.execute(httpGet);
+                    if (response.getStatusLine().getStatusCode() != 200) {
+                        if (httpGet.toString().contains("?keys"))
+                            log.debug("No workers running. Continue with master");
+                    } else {
+                        result = EntityUtils.toString(response.getEntity());
+                    }
+                } finally {
+                    response.close();
+                }
+            }
+        } finally {
+            return result;
+        }
+    }
+
+    private String deleteFromConsul(String query) throws IOException {
+        String result = null;
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        String consulURL = System.getenv("CONSULURL");
+        if (consulURL == null) throw new IOException("Consul url not set");
+        if (!consulURL.startsWith("http://")) {
+            consulURL = "http://" + consulURL;
+        }
+        try {
+            HttpDelete httpDelete;
+            httpDelete = new HttpDelete(consulURL + "/v1/kv/" + query);
+
+            //curl -X DELETE $CONSULURL/v1/kv/$DATASETS/$NODE_NAME
+            //curl -X DELETE $CONSULURL/v1/kv/$1/$NODE_NAME
+            log.debug("Running: " + httpDelete.getURI());
+            CloseableHttpResponse response = null;
+            if (httpDelete.toString().contains(System.getenv("EXAREME_ACTIVE_WORKERS_PATH")+"/") || httpDelete.toString().contains(System.getenv("DATASETS")+"/")) {    //if we can not contact : http://exareme-keystore:8500/v1/kv/master* or http://exareme-keystore:8500/v1/kv/datasets*
+                try {   //then throw exception
+                    response = httpclient.execute(httpDelete);
+                    if (response.getStatusLine().getStatusCode() != 200) {
+                        throw new ServerException("Cannot contact consul", new Exception(EntityUtils.toString(response.getEntity())));
+                    } else {
+                        result = EntityUtils.toString(response.getEntity());
+                    }
+                } finally {
+                    response.close();
+                }
+            }
+        }finally {
+            return result;
+        }
     }
 
     @Override
