090d5d1d5dab51f7a711eae9b8ccda3722eb588d
sofiakarb <karvoun@di.uoa.gr>
2019-09-30 19:27:38 +0300

    minor fixes

--- Deployment/Docker-Ansible/restart.sh
+++ Deployment/Docker-Ansible/restart.sh
@@ -12,15 +12,12 @@ read answer
 while true
 do
     if [[ "${answer}" == "1" ]]; then
-        echo -e "\nStopping Exareme services..."
         stop 1
         break
     elif [[ "${answer}" == "2" ]]; then
-        echo -e "\nStopping Portainer services..."
 		stop 2
         break
     elif [[ "${answer}" == "3" ]]; then
-        echo -e "\nStopping all services..."
 		stop 3
         break
     else
--- Deployment/Docker-Ansible/stop.sh
+++ Deployment/Docker-Ansible/stop.sh
@@ -13,15 +13,15 @@ do
     if [[ "${answer}" == "1" ]]; then
         echo -e "\nStopping Exareme services..."
         stop 1
-
+        break
     elif [[ "${answer}" == "2" ]]; then
         echo -e "\nStopping Portainer services..."
         stop 2
-
+        break
     elif [[ "${answer}" == "3" ]]; then
         echo -e "\nStopping all services..."
         stop 3
-
+        break
     else
         echo "$answer is not a valid answer! Try again.. [ 1-2-3 ]"
         read answer
