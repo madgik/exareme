4df392bca9325f21b8f9e2d15179789a9423e5ab
sofiakarb <karvoun@di.uoa.gr>
2019-10-09 19:47:46 +0300

    Local deployment first commit

---
+++ Deployment/Local/deployLocal.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+# Key-Value Store
+EXAREME_KEYSTORE="exareme-keystore:8500"
+
+# Docker internal folder for the Exareme data
+DOCKER_DATA_FOLDER="/root/exareme/data/"
+
+# Portainer
+PORTAINER_PORT="9000"
+PORTAINER_IMAGE="portainer/portainer"
+PORTAINER_VERSION=":latest"
+PORTAINER_DATA=$pwd"/portainer"
+
+FEDERATION_ROLE="master"
+
+#TODO maybe write it in a file?
+echo "What is the data_path for host machine?"
+read answer
+#Check that path ends with /
+if [[ "${answer: -1}"  != "/" ]]; then
+        answer=${answer}"/"
+fi
+LOCAL_DATA_FOLDER=${answer}
+
+chmod 755 *.sh
+. ./exareme.sh
+
+if [[ $(docker node ls | grep "Error") == '' ]]; then
+    :
+else
+    echo -e "\nInitialize Swarm.."
+    docker swarm init --advertise-addr=$(wget http://ipinfo.io/ip -qO -)
+fi
+
+if [[ $(docker network ls | grep mip-local) == '' ]]; then
+    echo -e "\nInitialize Network"
+    docker network create \
+            --driver=overlay --opt encrypted  --subnet=10.20.30.0/24  --ip-range=10.20.30.0/24 --gateway=10.20.30.254 mip-local
+fi
+
+#Get hostname of node
+name=$(hostname)
+
+echo -e "\nUpdate label name for Swarm node "$name
+docker node update --label-add name=${name} ${name}
+echo -e "\n"
+
+while read -r line1 ; do
+    read -r line2
+    imageName=$(echo "$line1" | cut -d ':' -d ' ' -d '"' -f 2 -d '"')
+    tag=$(echo "$line2" | cut -d ':' -d ' ' -d '"' -f 2 -d '"')
+done < exareme.yaml
+
+env FEDERATION_NODE=${name} FEDERATION_ROLE=${FEDERATION_ROLE} EXAREME_IMAGE=${imageName}":"${tag} \
+EXAREME_KEYSTORE=${EXAREME_KEYSTORE} DOCKER_DATA_FOLDER=${DOCKER_DATA_FOLDER} \
+LOCAL_DATA_FOLDER=${LOCAL_DATA_FOLDER} \
+docker stack deploy -c docker-compose-master.yml ${name}
+
+#TODO remove service if already exists
+echo -e "\nDo you wish to run Portainer service? [ y/n ]"
+read answer
+
+while true
+do
+    if [[ ${answer} == "y" ]];then
+        . ./portainer.sh
+        break
+    elif [[ ${answer} == "n" ]]; then
+        break
+    else
+        echo ${answer}" is not a valid answer. Try again [ y/n ]"
+        read answer
+     fi
+done
---
+++ Deployment/Local/exareme.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+#Check if EXAREME image exists
+docker_image_exists() {
+    curl -s -f https://index.docker.io/v1/repositories/$1/tags/$2 >/dev/null
+}
+
+#update exareme.yaml file
+updateFile () {
+while true
+do
+    if [[ -f exareme.yaml ]]; then
+        rm -f exareme.yaml
+    fi
+    echo -e "\nType your EXAREME image name:"
+    read name
+    echo "EXAREME_IMAGE:" \"${name}\" >> exareme.yaml
+
+    echo -e "\nType your EXAREME image tag:"
+    read tag
+    echo "EXAREME_TAG:" \"${tag}\" >> exareme.yaml
+
+    echo -e "\nChecking if EXAREME image: "\"${name}":"${tag}\"" exists"
+
+    #docker image may exist in docker hub
+    if docker_image_exists ${name} ${tag}; then
+        echo "EXAREME Image exists. Continuing..."
+        break
+    #or locally..
+    elif [[ "$(docker images -q ${name}:${tag} 2> /dev/null)" != "" ]]; then
+        echo "EXAREME Image exists. Continuing..."
+        break
+    else
+        echo -e "\nEXAREME image does not exist! EXAREME image name should have a format like: \"hbpmip/exareme\". And EXAREME image tag should have a format like: \"latest\""
+    fi
+done
+
+}
+
+if [[ -f exareme.yaml ]]; then
+    echo -e "\nFile \"exareme.yaml\" seams that already exists.\n"
+    while read -r line1 ; do
+        read -r line2
+        name=$(echo "$line1" | cut -d ':' -d ' ' -d '"' -f 2 -d '"')
+        tag=$(echo "$line2" | cut -d ':' -d ' ' -d '"' -f 2 -d '"')
+    done < exareme.yaml
+    echo "EXAREME image that will be used is: "\"${name}":"${tag}\"
+    echo "Do you wish to change EXAREME image?[ y/n ]"
+    read answer
+
+    while true
+    do
+        if [[ ${answer} == "y" ]]; then
+            updateFile
+            break
+        elif [[ ${answer} == "n" ]]; then
+            echo -e "\nChecking if EXAREME image: "\"${name}":"${tag}"\" exists."
+            if docker_image_exists ${name} ${tag}; then
+                echo "EXAREME image exists. Continuing..."
+                break
+            elif [[ "$(docker images -q ${name}:${tag} 2> /dev/null)" != "" ]]; then
+                echo "EXAREME Image exists. Continuing..."
+                break
+            else    #if EXAREME image that already exists in exareme.yaml file does not exist in docker hub, update exareme.yaml file
+                echo "EXAREME image does not exist! EXAREME image name should have a format like: \"hbpmip/exareme\". EXAREME image tag should have a format like: \"latest\""
+                updateFile
+            fi
+            break
+        else
+            echo "$answer is not a valid answer! Try again.. [ y/n ]"
+            read answer
+        fi
+    done
+else
+    echo -e "\nCreating file for Exareme image and Exareme tag.."
+    updateFile
+fi
+
+sleep 2
+return
