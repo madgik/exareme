f515d6c135d473685b8ca65f19fda4b4d65a9c6b
sofiakarb <karvoun@di.uoa.gr>
2019-10-21 13:21:59 +0300

    write image name, image tag only if image exists....

--- Local-Deployment/exareme.sh
+++ Local-Deployment/exareme.sh
@@ -11,21 +11,25 @@ while true
 do
     echo -e "\nType your EXAREME image name:"
     read name
-    echo "EXAREME_IMAGE:" \"${name}\" >> exareme.yaml
 
     echo -e "\nType your EXAREME image tag:"
     read tag
-    echo "EXAREME_TAG:" \"${tag}\" >> exareme.yaml
 
     echo -e "\nChecking if EXAREME image: "\"${name}":"${tag}\"" exists"
 
     #docker image may exist in docker hub
     if docker_image_exists ${name} ${tag}; then
         echo "EXAREME Image exists. Continuing..."
+        #If image exists in DockerHub write it to file exareme.yaml
+        echo "EXAREME_IMAGE:" \"${name}\" >> exareme.yaml
+        echo "EXAREME_TAG:" \"${tag}\" >> exareme.yaml
         break
     #or locally..
     elif [[ "$(sudo docker images -q ${name}:${tag} 2> /dev/null)" != "" ]]; then
         echo "EXAREME Image exists. Continuing..."
+        #if image exists locally write it to file exareme.yaml
+        echo "EXAREME_IMAGE:" \"${name}\" >> exareme.yaml
+        echo "EXAREME_TAG:" \"${tag}\" >> exareme.yaml
         break
     else
         echo -e "\nEXAREME image does not exist! EXAREME image name should have a format like: \"hbpmip/exareme\". And EXAREME image tag should have a format like: \"latest\""
