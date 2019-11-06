1cd93df10b47a9cf7963c1d01d6363a8b41abdf9
sofiakarb <karvoun@di.uoa.gr>
2019-11-06 15:30:53 +0200

    initial commit for storing pathology/datasets in consul

--- Exareme-Docker/files/root/exareme/set-local-datasets.sh
+++ Exareme-Docker/files/root/exareme/set-local-datasets.sh
@@ -3,18 +3,17 @@
 ALL_DATASETS=""
 MADIS="/root/exareme/lib/madis/src/mterm.py"
 
-for PATHOLOGY in $DOCKER_DATA_FOLDER/*	
+for PATHOLOGY in ${DOCKER_DATA_FOLDER}/*
 do
-	if [ -f $PATHOLOGY/datasets.csv ]
+	if [[ -f ${PATHOLOGY}/datasets.csv ]]
 	then
 		PATHOLOGY_DATASETS=$(echo "select distinct dataset from (file header:t  file:$PATHOLOGY/datasets.csv);" | $MADIS | sed '1d ; $d' ) 
-		
-		ALL_DATASETS+=" $PATHOLOGY_DATASETS"
+
+		PATHOLOGY_DATASETS=$(echo ${PATHOLOGY_DATASETS} | jq .[]  | sed 's/^\"//g ; s/\"$//g' | printf %s "$(cat)" | jq -R -c -s 'split("\n")')
+
+        curl -s -X PUT -d @- ${CONSULURL}/v1/kv/datasets/${NODE_NAME}/${PATHOLOGY} <<< ${PATHOLOGY_DATASETS}
+
+        PATHOLOGY_DATASETS=''
 	fi
 
 done
-
-
-ALL_DATASETS=$(echo $ALL_DATASETS | jq .[]  | sed 's/^\"//g ; s/\"$//g' | printf %s "$(cat)" | jq -R -c -s 'split("\n")')
-
-curl -s -X PUT -d @- $CONSULURL/v1/kv/datasets/$NODE_NAME <<< $ALL_DATASETS
