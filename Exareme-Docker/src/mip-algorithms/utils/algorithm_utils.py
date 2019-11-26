5468f136f9fdcac13499880c3eb62cb008caa642
Jason Sakellariou <js.sakel@gmail.com>
2019-11-26 20:34:08 +0200

    Formula patser first commit

--- Exareme-Docker/src/mip-algorithms/utils/algorithm_utils.py
+++ Exareme-Docker/src/mip-algorithms/utils/algorithm_utils.py
@@ -7,7 +7,9 @@ import codecs
 import logging
 import os
 import errno
-
+import pandas as pd
+import numpy as np
+from patsy import dmatrix, dmatrices
 
 PRIVACY_MAGIC_NUMBER = 10
 P_VALUE_CUTOFF = 0.001
@@ -48,6 +50,40 @@ def query_with_privacy(fname_db, query):
     return schema, data
 
 
+def query_from_formula(fname_db, formula, variables):
+    from numpy import log as log
+    from numpy import exp as exp
+    conn = sqlite3.connect(fname_db)
+    # Perform privacy check
+    if pd.read_sql_query(make_count_query(variables), conn).iat[0, 0] < PRIVACY_MAGIC_NUMBER:
+        raise PrivacyError('Query results in illegal number of datapoints.')
+        # TODO privacy check by variable
+    # Pull data and return design matrix(-ces)
+    data = pd.read_sql_query(make_data_query(variables), conn)
+    if '~' in formula:
+        Y, X = dmatrices(formula, data, return_type='dataframe')
+        return Y, X
+    else:
+        X = dmatrix(formula, data, return_type='dataframe')
+        return X
+
+
+def make_count_query(variables):
+    q = 'SELECT count(' + variables[0] + ') FROM DATA WHERE '
+    q += ' AND '.join([v + "!=''" for v in variables])
+    q += ';'
+    return q
+
+
+def make_data_query(variables):
+    q = 'SELECT '
+    q += ', '.join(variables)
+    q += ' FROM DATA WHERE '
+    q += ' AND '.join([v + "!=''" for v in variables])
+    q += ';'
+    return q
+
+
 class StateData(object):
     def __init__(self, **kwargs):
         self.data = kwargs
@@ -78,6 +114,7 @@ class StateData(object):
                 raise
         return obj
 
+
 def init_logger():
     logging.basicConfig(filename='/var/log/exaremePythonAlgorithms.log')
 
@@ -94,3 +131,14 @@ class PrivacyError(Exception):
 class ExaremeError(Exception):
     def __init__(self, message):
         super(ExaremeError, self).__init__(message)
+
+
+if __name__ == '__main__':
+    fname_db = '/Users/zazon/madgik/mip_data/dementia/datasets.db'
+    variables = ['lefthippocampus', 'leftamygdala', 'gender', 'righthippocampus']
+    formula = "gender ~ (lefthippocampus + leftamygdala) * righthippocampus + log(leftamygdala) + standardize(" \
+              "righthippocampus) + exp(lefthippocampus) + I(lefthippocampus + leftamygdala*2)"
+    Y, X = query_from_formula(fname_db, formula, variables)
+    print(Y.design_info.term_names)
+    print(X.design_info.term_names)
+    print(X)
