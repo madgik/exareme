43826560664cb8a1c7ce5ffdb8e5ff982e338ba9
Jason Sakellariou <js.sakel@gmail.com>
2020-01-09 10:45:11 +0200

    Create template for multiple-local-global algos

---
+++ Exareme-Docker/src/mip-algorithms/TEMPLATE_MULTIPLE_LOCAL_GLOBAL/1/global.py
@@ -0,0 +1,39 @@
+from __future__ import division
+from __future__ import print_function
+
+import sys
+from os import path
+
+_new_path = path.dirname(path.dirname(path.abspath(__file__)))
+sys.path.append(_new_path)
+while True:
+    try:
+        import utils.algorithm_utils
+    except:
+        sys.path.pop()
+        _new_path = path.dirname(_new_path)
+        sys.path.append(_new_path)
+    else:
+        break
+del _new_path
+
+from utils.algorithm_utils import TransferAndAggregateData, parse_exareme_args
+from TEMPLATE_MULTIPLE_LOCAL_GLOBAL.algorithm_lib import global_1
+
+
+def main(args):
+    local_dbs = path.abspath(args.local_step_dbs)
+    fname_cur_state = path.abspath(args.cur_state_pkl)
+
+    local_out = TransferAndAggregateData.load(local_dbs)
+    # Run algorithm global step
+    global_state, global_out = global_1(global_in=local_out)
+    # Save global state
+    global_state.save(fname=fname_cur_state)
+    # Return the algorithm's output
+    global_out.transfer()
+
+
+if __name__ == '__main__':
+    args = parse_exareme_args(__file__)
+    main(args)
---
+++ Exareme-Docker/src/mip-algorithms/TEMPLATE_MULTIPLE_LOCAL_GLOBAL/1/local.py
@@ -0,0 +1,39 @@
+# Forward compatibility
+from __future__ import division
+from __future__ import print_function
+
+import sys
+from os import path
+
+_new_path = path.dirname(path.dirname(path.abspath(__file__)))
+sys.path.append(_new_path)
+while True:
+    try:
+        import utils.algorithm_utils
+    except:
+        sys.path.pop()
+        _new_path = path.dirname(_new_path)
+        sys.path.append(_new_path)
+    else:
+        break
+del _new_path
+
+from utils.algorithm_utils import parse_exareme_args
+from TEMPLATE_MULTIPLE_LOCAL_GLOBAL.algorithm_lib import get_data, local_1
+
+
+def main(args):
+    fname_cur_state = path.abspath(args.cur_state_pkl)
+
+    local_in = get_data(args)
+    # Run algorithm local step
+    local_state, local_out = local_1(local_in=local_in)
+    # Save local state
+    local_state.save(fname=fname_cur_state)
+    # Transfer local output
+    local_out.transfer()
+
+
+if __name__ == '__main__':
+    args = parse_exareme_args(__file__)
+    main(args)
