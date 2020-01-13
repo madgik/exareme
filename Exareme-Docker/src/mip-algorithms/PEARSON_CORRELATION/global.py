ea9df1f2f663bf61e67ca1288eb6a98300fabce6
Jason Sakellariou <js.sakel@gmail.com>
2020-01-13 19:02:14 +0200

    Refactor algorithm structure

--- Exareme-Docker/src/mip-algorithms/PEARSON_CORRELATION/global.py
+++ Exareme-Docker/src/mip-algorithms/PEARSON_CORRELATION/global.py
@@ -2,225 +2,35 @@ from __future__ import division
 from __future__ import print_function
 
 import sys
-import math
-import json
 from os import path
-import numpy as np
-import scipy.special as special
-import scipy.stats as st
-from argparse import ArgumentParser
 
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))) + '/utils/')
-
-from algorithm_utils import set_algorithms_output_data, ExaremeError, P_VALUE_CUTOFF, P_VALUE_CUTOFF_STR
-from pearsonc_lib import PearsonCorrelationLocalDT
-
-
-def pearsonr_global(global_in):
-    """Global step in Pearson correlation coefficient. Local statistics, computed in local step, are aggregated and
-    then Pearson correlation coefficient `r`, p-value `prob` and lower and upper confidence intervals at 95% `ci_lo`
-    and `ci_hi` are computed. Pearson correlation is computed according to standard formula
-    (see https://en.wikipedia.org/wiki/Pearson_correlation_coefficient#For_a_sample). The p-value is computed using
-    the incomplete beta integral method. The lower and upper confidence intervals are computed usign the Fisher
-    information (see https://en.wikipedia.org/wiki/Pearson_correlation_coefficient#Using_the_Fisher_transformation).
-
-    Parameters
-    ----------
-    global_in : PearsonCorrelationLocalDT
-        Object holding aggregated values of statistics computed in local step.
-
-    Returns
-    -------
-    global_out : str
-        JSON string containing a list of results, one for each variable pair, where each result hold the variable
-        pair names, the Pearson coefficient, the p-value and the lower and upper confidence intervals.
-    """
-    n_obs, sx, sy, sxx, sxy, syy, cm_names, lnames, rnames = global_in.get_data()
-    cm_shape = sx.shape
-    r = np.zeros(cm_shape, dtype=np.float)
-    prob = np.zeros(cm_shape, dtype=np.float)
-    ci_lo = np.zeros(cm_shape, dtype=np.float)
-    ci_hi = np.zeros(cm_shape, dtype=np.float)
-
-    df = n_obs - 2
-    for i in xrange(cm_shape[0]):
-        for j in xrange(cm_shape[1]):
-            d = (math.sqrt(n_obs * sxx[i, j] - sx[i, j] * sx[i, j])
-                 * math.sqrt(n_obs * syy[i, j] - sy[i, j] * sy[i, j]))
-            if d == 0:
-                r[i, j] = 0
-            else:
-                r[i, j] = float((n_obs * sxy[i, j] - sx[i, j] * sy[i, j]) / d)
-            r[i, j] = max(min(r[i, j], 1.0), -1.0)  # If abs(r) > 1 correct it: artifact of floating point arithmetic.
-            if abs(r[i, j]) == 1.0:
-                prob[i, j] = 0.0
-            else:
-                t_squared = r[i, j] ** 2 * (df / ((1.0 - r[i, j]) * (1.0 + r[i, j])))
-                prob[i, j] = special.betainc(
-                        0.5 * df, 0.5, np.fmin(np.asarray(df / (df + t_squared)), 1.0)
-                )
-            # Compute 95% confidence intervals
-            alpha = 0.05  # Two-tail test with confidence intervals 95%
-            if r[i, j] is not None:
-                r_z = np.arctanh(r[i, j])
-                se = 1 / np.sqrt(n_obs - 3)
-                z = st.norm.ppf(1 - alpha / 2)
-                lo_z, hi_z = r_z - z * se, r_z + z * se
-                ci_lo[i, j], ci_hi[i, j] = np.tanh((lo_z, hi_z))
-            else:
-                raise ExaremeError('Pearson coefficient is NaN.')
-
-    # Format output data
-    # JSON raw
-    result_list = []
-    for i in xrange(cm_shape[0]):
-        for j in xrange(cm_shape[1]):
-            result_list.append({
-                'Variables'                      : cm_names[i, j],
-                'Pearson correlation coefficient': r[i, j],
-                'p-value'                        : prob[i, j] if prob[i, j] >= P_VALUE_CUTOFF else P_VALUE_CUTOFF_STR,
-                'C.I. Lower'                     : ci_lo[i, j],
-                'C.I. Upper'                     : ci_hi[i, j]
-            })
-    # Tabular summary
-    tabular_data_summary = [["variables", "Pearson correlation coefficient", "p-value", "lower c.i.", "upper c.i."]]
-    for i in xrange(cm_shape[0]):
-        for j in xrange(cm_shape[1]):
-            tabular_data_summary.append([
-                cm_names[i, j],
-                r[i, j],
-                prob[i, j] if prob[i, j] >= P_VALUE_CUTOFF else P_VALUE_CUTOFF_STR,
-                ci_lo[i, j],
-                ci_hi[i, j]
-            ])
-    tabular_data_summary_schema_fields = [
-        {
-            "name": "variables",
-            "type": "string"
-        }, {
-            "name": "Pearson correlation coefficient",
-            "type": "number"
-        }, {
-            "name": "p-value",
-            "type": "string"
-        }, {
-            "name": "lower c.i.",
-            "type": "number"
-        }, {
-            "name": "upper c.i.",
-            "type": "number"
-        },
-    ]
-    # Highchart Correlation Matrix
-    correlmatr_data = []
-    for i in xrange(cm_shape[0]):
-        for j in xrange(cm_shape[1]):
-            correlmatr_data.append({
-                'x'    : j,
-                'y'    : i,
-                'value': round(r[i, j], 4),
-                'name' : cm_names[i, j]
-            })
-
-    hichart_correl_matr = {
-        'chart'    : {
-            'type'           : 'heatmap',
-            'plotBorderWidth': 1
-        },
-        'title'    : {
-            'text': 'Pearson Correlation Matrix'
-        },
-        'xAxis'    : {
-            'categories': rnames
-        },
-        'yAxis'    : {
-            'categories': lnames,
-            'title'     : 'null'
-        },
-        'colorAxis': {
-            'stops'   : [
-                [0, '#c4463a'],
-                [0.5, '#ffffff'],
-                [0.9, '#3060cf']
-            ],
-            'min'     : -1,
-            'max'     : 1,
-            'minColor': '#FFFFFF',
-            'maxColor': "#6699ff"
-        },
-        'legend'   : {
-            'align'        : 'right',
-            'layout'       : 'vertical',
-            'margin'       : 0,
-            'verticalAlign': 'top',
-            'y'            : 25,
-            'symbolHeight' : 280
-        },
-        'tooltip'  : {
-            'headerFormat': '',
-            'pointFormat' : '<b>{point.name}: {point.value}</b>',
-            'enabled'     : True
-        },
-        'series'   : [{
-            'name'       : 'coefficients',
-            'borderWidth': 1,
-            'data'       : correlmatr_data,
-            'dataLabels' : {
-                'enabled': True,
-                'color'  : '#000000'
-            }
-        }]
-    }
-    # Write output to JSON
-    result = {
-        'result': [
-            # Raw results
-            {
-                "type": "application/json",
-                "data": result_list
-            },
-            # Tabular data resource summary
-            {
-                "type": "application/vnd.dataresource+json",
-                "data":
-                    {
-                        "name"   : "Pearson correlation summary",
-                        "profile": "tabular-data-resource",
-                        "data"   : tabular_data_summary[1:],
-                        "schema" : {
-                            "fields": tabular_data_summary_schema_fields
-                        }
-                    }
-
-            },
-            # Highchart correlation matrix
-            {
-                "type": "application/vnd.highcharts+json",
-                "data": hichart_correl_matr
-            }
-        ]
-    }
+_new_path = path.dirname(path.dirname(path.abspath(__file__)))
+sys.path.append(_new_path)
+while True:
     try:
-        global_out = json.dumps(result, allow_nan=False)
-    except ValueError:
-        print('Result contains NaNs.')
+        import utils.algorithm_utils
+    except:
+        sys.path.pop()
+        _new_path = path.dirname(_new_path)
+        sys.path.append(_new_path)
     else:
-        return global_out
+        break
+del _new_path
+
+from utils.algorithm_utils import parse_exareme_args, set_algorithms_output_data
+from pearsonc_lib import pearson_global, PearsonCorrelationLocalDT
 
 
-def main():
-    # Parse arguments
-    parser = ArgumentParser()
-    parser.add_argument('-local_step_dbs', required=True, help='Path to local db.')
-    args, unknown = parser.parse_known_args()
+def main(args):
     local_dbs = path.abspath(args.local_step_dbs)
 
     local_out = PearsonCorrelationLocalDT.load(local_dbs)
     # Run algorithm global step
-    global_out = pearsonr_global(global_in=local_out)
+    global_out = pearson_global(global_in=local_out)
     # Return the algorithm's output
     set_algorithms_output_data(global_out)
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_exareme_args(__file__)
+    main(args)
--- Exareme-Docker/src/mip-algorithms/PEARSON_CORRELATION/local.py
+++ Exareme-Docker/src/mip-algorithms/PEARSON_CORRELATION/local.py
@@ -2,128 +2,34 @@
 from __future__ import division
 from __future__ import print_function
 
-import json
 import sys
-from argparse import ArgumentParser
 from os import path
 
-import numpy as np
-
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))) + '/utils/')
-
-from algorithm_utils import query_from_formula
-from pearsonc_lib import PearsonCorrelationLocalDT
-
-
-def pearsonr_local(local_in):
-    # TODO Rewrite docstring
-    """Local step in Pearson correlation coefficient. Statistics are computed in each local database and then they
-    are sent to the master node to be aggregated accordingly.
-    The computed statistics are: `nn` number of observations, `sx` and `sy` sums of linear terms \sum_i x_i and
-    \sum_i y_i respectively, `sxx`, `sxy` and `syy` sums of quadratic terms \sum_i x_i x_i, \sum_i x_i y_i and
-    \sum_i y_i y_i respectively, where x and y are vectors of the pair of variables under consideration, pulled from
-    matrices right_vars and left_vars respectively.
-
-    Parameters
-    ----------
-    local_in : numpy.array, numpy.array, list, list
-        Tuple holding matrices right_vars and left_vars as numpy arrays and lists of variable names schema_X and schema_Y
-
-    Returns
-    -------
-    local_out: PearsonCorrelationLocalDT
-       Object holding the computed statistics as well as schema_X, schema_Y do be transferred to the master node.
-    """
-    # Unpack data
-    left_vars, right_vars = local_in
-
-    n_obs = len(right_vars)
-    cm_shape = len(left_vars.columns), len(right_vars.columns)
-
-    # Compute statistics
-    sx = np.empty(cm_shape, dtype=np.float)
-    sy = np.empty(cm_shape, dtype=np.float)
-    sxx = np.empty(cm_shape, dtype=np.float)
-    sxy = np.empty(cm_shape, dtype=np.float)
-    syy = np.empty(cm_shape, dtype=np.float)
-    cm_names = np.array([[''] * cm_shape[1]] * cm_shape[0], dtype=object)
-    for i in xrange(cm_shape[0]):
-        for j in xrange(cm_shape[1]):
-            y, x = left_vars.iloc[:, i], right_vars.iloc[:, j]
-            sx[i, j] = x.sum()
-            sy[i, j] = y.sum()
-            sxx[i, j] = (x * x).sum()
-            sxy[i, j] = (x * y).sum()
-            syy[i, j] = (y * y).sum()
-            cm_names[i, j] = y.name + ' ~ ' + x.name
-
-    local_out = PearsonCorrelationLocalDT(
-            n_obs, sx, sy, sxx, sxy, syy, cm_names, list(left_vars.columns), list(right_vars.columns))
-
-    return local_out
-
-
-def main():
-    # Parse arguments
-    parser = ArgumentParser()
-    # Algo arguments
-    parser.add_argument('-x', required=True, help='Variable names in x, comma separated.')
-    parser.add_argument('-y', required=True, help='Variable names in y, comma separated.')
-    parser.add_argument('-dataset', required=True, help='Dataset name.')
-    parser.add_argument('-formula', required=True, help='A string holding a patsy formula.')
-    parser.add_argument('-no_intercept', required=True, help='A boolean signaling a no-intercept-by-default behaviour.')
-    parser.add_argument('-coding', required=True, help='Coding method for categorical variables.')
-    # Exareme arguments
-    parser.add_argument('-input_local_DB', required=True, help='Path to local db.')
-    parser.add_argument('-data_table', required=True)
-    parser.add_argument('-metadata_table', required=True)
-    parser.add_argument('-metadata_code_column', required=True)
-    parser.add_argument('-metadata_isCategorical_column', required=True)
-
-    args, unknown = parser.parse_known_args()
-
-    args_y = list(
-            args.y
-                .replace(' ', '')
-                .split(',')
-    )
-    if args.x == '':
-        variables = (args_y,)
+_new_path = path.dirname(path.dirname(path.abspath(__file__)))
+sys.path.append(_new_path)
+while True:
+    try:
+        import utils.algorithm_utils
+    except:
+        sys.path.pop()
+        _new_path = path.dirname(_new_path)
+        sys.path.append(_new_path)
     else:
-        args_x = list(
-                args.x
-                    .replace(' ', '')
-                    .split(',')
-        )
-        variables = (args_y, args_x)
+        break
+del _new_path
 
-    dataset = args.dataset
-    formula = args.formula
-    formula = formula.replace('_', '~')  # TODO Fix tilda problem and remove
-    no_intercept = json.loads(args.no_intercept)
-    input_local_DB = args.input_local_DB
-    data_table = args.data_table
-    metadata_table = args.metadata_table
-    metadata_code_column = args.metadata_code_column
-    metadata_isCategorical_column = args.metadata_isCategorical_column
-    coding = None if args.coding == 'null' else args.coding
-    left_vars, right_vars = query_from_formula(fname_db=input_local_DB, formula=formula, variables=variables,
-                                               dataset=dataset,
-                                               data_table=data_table, metadata_table=metadata_table,
-                                               metadata_code_column=metadata_code_column,
-                                               metadata_isCategorical_column=metadata_isCategorical_column,
-                                               no_intercept=no_intercept, coding=coding)
+from utils.algorithm_utils import parse_exareme_args
+from pearsonc_lib import get_data, pearson_local
 
-    if left_vars is None:
-        left_vars = right_vars
-    local_in = left_vars, right_vars
 
+def main(args):
+    local_in = get_data(args)
     # Run algorithm local step
-    local_out = pearsonr_local(local_in=local_in)
-
+    local_out = pearson_local(local_in=local_in)
     # Return the output data (should be the last command)
     local_out.transfer()
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_exareme_args(__file__)
+    main(args)
