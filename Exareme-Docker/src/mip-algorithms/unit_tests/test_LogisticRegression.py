1b46034b7f312afdfb5cc3de1cde3f8b48eae40d
sofiakarb <karvoun@di.uoa.gr>
2020-02-26 16:17:24 +0200

    test IP

--- Exareme-Docker/src/mip-algorithms/unit_tests/test_LogisticRegression.py
+++ Exareme-Docker/src/mip-algorithms/unit_tests/test_LogisticRegression.py
@@ -6,7 +6,7 @@ import math
 # Required datasets: data_logisticRegression.csv
 
 
-endpointUrl = 'http://88.197.53.38:9090/mining/query/LOGISTIC_REGRESSION'
+endpointUrl = 'http://88.197.53.100:9090/mining/query/LOGISTIC_REGRESSION'
 
 
 def test_LogisticRegression():
