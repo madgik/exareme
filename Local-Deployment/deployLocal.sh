1cd8ff0587e853e9774b55d6c3cae27bf97a2401
sofiakarb <karvoun@di.uoa.gr>
2020-03-03 10:49:35 +0200

    beauty

--- Local-Deployment/deployLocal.sh
+++ Local-Deployment/deployLocal.sh
@@ -58,13 +58,13 @@ echo "Starting the process of creating databases.."
 echo -e "\nDeleting previous databases."
 rm -rf ${LOCAL_DATA_FOLDER}/**/*.db
 
-echo "Parsing csv files in " ${LOCAL_DATA_FOLDER} " to database files. "
+echo -e "\nParsing csv files in " ${LOCAL_DATA_FOLDER} " to database files. "
 python ../Exareme-Docker/files/root/exareme/convert-csv-dataset-to-db.py -f ${LOCAL_DATA_FOLDER} -t "master"
 #Get the status code from previous command
 py_script=$?
 #If status code != 0 an error has occurred
 if [[ ${py_script} -ne 0 ]]; then
-     echo "Creation of databases failed. Exareme will not run until fixes be made." >&2
+     echo -e "\nCreation of databases failed. Exareme will not run until fixes be made." >&2
      exit 1
 fi
 
