d05bd3411334ad414f323db1d5b5d94b76094062
Jason Sakellariou <js.sakel@gmail.com>
2020-04-07 00:52:34 +0300

    Refactor Pearson with numpy broadcasting

--- Exareme-Docker/src/mip-algorithms/PEARSON_CORRELATION/pearson.py
+++ Exareme-Docker/src/mip-algorithms/PEARSON_CORRELATION/pearson.py
@@ -6,8 +6,8 @@ import numpy as np
 import scipy.special as special
 import scipy.stats as st
 
-from mipframework import Algorithm, AlgorithmResult, TabularDataResource, AlgorithmError
-from mipframework.constants import P_VALUE_CUTOFF, P_VALUE_CUTOFF_STR
+from mipframework import Algorithm, AlgorithmResult, TabularDataResource
+from mipframework.constants import P_VALUE_CUTOFF, P_VALUE_CUTOFF_STR, CONFIDENCE
 from mipframework.highcharts import CorrelationHeatmap
 
 
@@ -16,30 +16,21 @@ class Pearson(Algorithm):
         super(Pearson, self).__init__(__file__, cli_args, intercept=False)
 
     def local_(self):
-        left_vars = right_vars = self.data.variables
+        n_obs = self.data.variables.shape[0]
+        Y = X = self.data.variables.to_numpy()
+        y_names = x_names = self.data.variables.columns.to_numpy()[:, np.newaxis]
         if self.data.covariables is not None:
-            right_vars = self.data.covariables
+            X = self.data.covariables.to_numpy()
+            x_names = self.data.covariables.columns.to_numpy()
 
-        n_obs = len(right_vars)
-        cm_shape = len(left_vars.columns), len(right_vars.columns)
-
-        # Compute statistics
-        sx = np.empty(cm_shape, dtype=np.float)
-        sy = np.empty(cm_shape, dtype=np.float)
-        sxx = np.empty(cm_shape, dtype=np.float)
-        sxy = np.empty(cm_shape, dtype=np.float)
-        # sxy = (X * X.T[:, :, None]).sum(axis=1)
-        syy = np.empty(cm_shape, dtype=np.float)
-        cm_names = np.array([[''] * cm_shape[1]] * cm_shape[0], dtype=object)
-        for i in xrange(cm_shape[0]):
-            for j in xrange(cm_shape[1]):
-                y, x = left_vars.iloc[:, i], right_vars.iloc[:, j]
-                sx[i, j] = x.sum()
-                sy[i, j] = y.sum()
-                sxx[i, j] = (x * x).sum()
-                sxy[i, j] = (x * y).sum()
-                syy[i, j] = (y * y).sum()
-                cm_names[i, j] = y.name + ' ~ ' + x.name
+        sx = X.sum(axis=0)
+        sy = Y.sum(axis=0)
+        sxx = (X ** 2).sum(axis=0)
+        sxy = (X * Y.T[:, :, np.newaxis]).sum(axis=1)
+        syy = (Y ** 2).sum(axis=0)
+        tildas = np.empty(sxy.shape, dtype=object)
+        tildas[:] = " ~ "
+        pair_names = y_names + tildas + x_names
 
         self.push_and_add(n_obs=n_obs)
         self.push_and_add(sx=sx)
@@ -47,117 +38,111 @@ class Pearson(Algorithm):
         self.push_and_add(sxx=sxx)
         self.push_and_add(sxy=sxy)
         self.push_and_add(syy=syy)
-        self.push_and_agree(cm_names=cm_names)
-        self.push_and_agree(lnames=list(left_vars.columns))
-        self.push_and_agree(rnames=list(right_vars.columns))
+        self.push_and_agree(pair_names=pair_names)
 
     def global_(self):
-        n_obs = self.fetch('n_obs')
-        sx = self.fetch('sx')
-        sy = self.fetch('sy')
-        sxx = self.fetch('sxx')
-        sxy = self.fetch('sxy')
-        syy = self.fetch('syy')
-        cm_names = self.fetch('cm_names')
-        lnames = self.fetch('lnames')
-        rnames = self.fetch('rnames')
-
-        cm_shape = sx.shape
-        r = np.zeros(cm_shape, dtype=np.float)
-        prob = np.zeros(cm_shape, dtype=np.float)
-        ci_lo = np.zeros(cm_shape, dtype=np.float)
-        ci_hi = np.zeros(cm_shape, dtype=np.float)
+        n_obs = self.fetch("n_obs")
+        sx = self.fetch("sx")
+        sy = self.fetch("sy")
+        sxx = self.fetch("sxx")
+        sxy = self.fetch("sxy")
+        syy = self.fetch("syy")
+        pair_names = self.fetch("pair_names")
 
         df = n_obs - 2
-        for i in range(cm_shape[0]):
-            for j in range(cm_shape[1]):
-                d = (np.sqrt(n_obs * sxx[i, j] - sx[i, j] * sx[i, j])
-                     * np.sqrt(n_obs * syy[i, j] - sy[i, j] * sy[i, j]))
-                if d == 0:
-                    r[i, j] = 0
-                else:
-                    r[i, j] = float((n_obs * sxy[i, j] - sx[i, j] * sy[i, j]) / d)
-                # If abs(r) > 1 artifact of floating point arithmetic.
-                r[i, j] = max(min(r[i, j], 1.0), -1.0)
-                if abs(r[i, j]) == 1.0:
-                    prob[i, j] = 0.0
-                else:
-                    t_squared = r[i, j] ** 2 * (df / ((1.0 - r[i, j]) * (1.0 + r[i, j])))
-                    prob[i, j] = special.betainc(
-                        0.5 * df, 0.5, np.fmin(np.asarray(df / (df + t_squared)), 1.0)
-                    )
-                # Compute 95% confidence intervals
-                alpha = 0.05  # Two-tail test with confidence intervals 95%
-                if r[i, j] is not None:
-                    r_z = np.arctanh(r[i, j])
-                    se = 1 / np.sqrt(n_obs - 3)
-                    z = st.norm.ppf(1 - alpha / 2)
-                    lo_z, hi_z = r_z - z * se, r_z + z * se
-                    ci_lo[i, j], ci_hi[i, j] = np.tanh((lo_z, hi_z))
-                else:
-                    raise AlgorithmError('Pearson coefficient is NaN.')
+        d = (
+            np.sqrt(n_obs * sxx - sx * sx)
+            * np.sqrt(n_obs * syy - sy * sy)[:, np.newaxis]
+        )
+        r = (n_obs * sxy - sx * sy[:, np.newaxis]) / d
+        r[d == 0] = 0
+        r = r.clip(-1, 1)
+        t_squared = r ** 2 * (df / ((1.0 - r) * (1.0 + r)))
+        prob = special.betainc(
+            0.5 * df, 0.5, np.fmin(np.asarray(df / (df + t_squared)), 1.0)
+        )
+        prob[abs(r) == 1] = 0
+        r_z = np.arctanh(r)
+        se = 1 / np.sqrt(n_obs - 3)
+        alpha = 1 - CONFIDENCE
+        z = st.norm.ppf(1 - alpha / 2)
+        lo_z, hi_z = r_z - z * se, r_z + z * se
+        ci_lo, ci_hi = np.tanh((lo_z, hi_z))
 
         # Format results for output
         raw_data = {
-            'n_obs'                          : n_obs,
-            'Correlation matrix labels'      : cm_names.tolist(),
-            'x axis names'                   : rnames,
-            'y axis names'                   : lnames,
-            'Pearson correlation coefficient': r.tolist(),
-            'p-value'                        : prob.tolist(),
-            'C.I. Lower'                     : ci_lo.tolist(),
-            'C.I. Upper'                     : ci_hi.tolist()
+            "n_obs": n_obs,
+            "Correlation matrix labels": pair_names.tolist(),
+            "Pearson correlation coefficient": r.tolist(),
+            "p-value": prob.tolist(),
+            "C.I. Lower": ci_lo.tolist(),
+            "C.I. Upper": ci_hi.tolist(),
         }
         correl_tabular = []
-        for i in range(cm_shape[0]):
-            for j in range(cm_shape[1]):
-                correl_tabular.append([
-                    cm_names[i, j],
-                    r[i, j],
-                    (str(prob[i, j])
-                     if prob[i, j] >= P_VALUE_CUTOFF
-                     else P_VALUE_CUTOFF_STR),
-                    ci_lo[i, j],
-                    ci_hi[i, j]
-                ])
+        for i in range(sxy.shape[0]):
+            for j in range(sxy.shape[1]):
+                correl_tabular.append(
+                    [
+                        pair_names[i, j],
+                        r[i, j],
+                        (
+                            str(prob[i, j])
+                            if prob[i, j] >= P_VALUE_CUTOFF
+                            else P_VALUE_CUTOFF_STR
+                        ),
+                        ci_lo[i, j],
+                        ci_hi[i, j],
+                    ]
+                )
         table_output = TabularDataResource(
             fields=[
-                'variables',
-                'Pearson correlation coefficient',
-                'p-value',
-                'lower c.i.',
-                'upper c.i.'
+                "variables",
+                "Pearson correlation coefficient",
+                "p-value",
+                "lower c.i.",
+                "upper c.i.",
             ],
             data=correl_tabular,
-            title='Pearson Correlation Summary')
+            title="Pearson Correlation Summary",
+        )
 
-        highchart = CorrelationHeatmap(title='Pearson Correlation Heatmap', matrix=r,
-                                       min_val=-1, max_val=1)
+        highchart = CorrelationHeatmap(
+            title="Pearson Correlation Heatmap", matrix=r, min_val=-1, max_val=1
+        )
 
         self.result = AlgorithmResult(
-            raw_data=raw_data,
-            tables=[table_output],
-            highcharts=[highchart]
+            raw_data=raw_data, tables=[table_output], highcharts=[highchart]
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import time
     from mipframework import create_runner
 
     algorithm_args = [
-        '-x', 'rightioginferioroccipitalgyrus,rightmfcmedialfrontalcortex',
-        '-y', 'subjectage,rightventraldc,rightaccumbensarea',
-        '-pathology', 'dementia, leftaccumbensarea',
-        '-dataset', 'adni',
-        '-filter', '',
-        '-formula', '',
-        '-coding', '',
+        "-x",
+        "rightioginferioroccipitalgyrus,rightmfcmedialfrontalcortex",
+        "-y",
+        "subjectage,rightventraldc,rightaccumbensarea",
+        "-pathology",
+        "dementia, leftaccumbensarea",
+        "-dataset",
+        "adni",
+        "-filter",
+        "",
+        "-formula",
+        "",
+        "-coding",
+        "",
     ]
-    runner = create_runner(for_class='Pearson', found_in='PEARSON_CORRELATION/pearson',
-                           alg_type='local-global', algorithm_args=algorithm_args,
-                           num_workers=3)
+    runner = create_runner(
+        for_class="Pearson",
+        found_in="PEARSON_CORRELATION/pearson",
+        alg_type="local-global",
+        algorithm_args=algorithm_args,
+        num_workers=3,
+    )
     start = time.time()
     runner.run()
     end = time.time()
-    print('Completed in ', end - start)
+    print("Completed in ", end - start)
