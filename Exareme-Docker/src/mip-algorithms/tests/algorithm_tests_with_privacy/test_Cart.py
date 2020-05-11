341211fe27f9be565c7e107f3b8b3ec3ca1541d5
ezacharia <ezacharia@di.uoa.gr>
2020-05-11 14:57:11 +0300

    add Cart test for diabetes dataset

--- Exareme-Docker/src/mip-algorithms/tests/algorithm_tests_with_privacy/test_Cart.py
+++ Exareme-Docker/src/mip-algorithms/tests/algorithm_tests_with_privacy/test_Cart.py
@@ -11,10 +11,11 @@ import csv
 import pandas as pd
 from sklearn import tree
 from sklearn.tree import export_graphviz
-from sklearn.tree import DecisionTreeClassifier as SklearnDecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
 import itertools
 import numpy as np
-from sklearn.metrics import r2_score,mean_squared_error
+from sklearn import metrics
+#from sklearn.model_selection import train_test_split
 
 from os import path
 sys.path.append(path.abspath(__file__))
@@ -24,10 +25,19 @@ endpointUrl_CartTraining= vmUrl+'CART'
 endpointUrl_CartPredict= vmUrl+'CART_PREDICT'
 path = '../data/dementia/'
 
+# argsX = "montrealcognitiveassessment,"
+# argsX +="rightpcggposteriorcingulategyrus,leftpcggposteriorcingulategyrus,"
+# argsX +="rightacgganteriorcingulategyrus,"
+# argsX +="leftmcggmiddlecingulategyrus,"
+# argsX +="rightphgparahippocampalgyrus,"
+# argsX +="rightententorhinalarea,"
+# argsX +="righthippocampus,lefthippocampus,"
+# argsX+= "rightthalamusproper,leftthalamusproper"
 
 def run_sklearn_classification(trainingDatasetPath, PredictionDatasetPath, argsX, argsY, maxDepth):
+
     #1. Read training data
-    df = pd.read_csv(trainingDatasetPath, index_col ="subjectcode")
+    df = pd.read_csv(trainingDatasetPath)
     args_X = list(argsX.replace(" ", "").split(","))
     args_Y = [argsY.replace(" ", "")][0]
     varNames = [x for x in args_X]
@@ -38,12 +48,11 @@ def run_sklearn_classification(trainingDatasetPath, PredictionDatasetPath, argsX
     y = df[args_Y]
 
     #2. Run scikit tree classifier
-    clf = tree.DecisionTreeClassifier()
-    clf = SklearnDecisionTreeClassifier(max_depth = int(maxDepth))
+    clf = DecisionTreeClassifier(random_state=0, max_depth = int(maxDepth))
     clf = clf.fit(X, y)
 
     #3. Read testing dataset
-    df = pd.read_csv(PredictionDatasetPath, index_col ="subjectcode")
+    df = pd.read_csv(PredictionDatasetPath)
     df = df[varNames]
     df = df.dropna()
     X = df.drop(args_Y, axis=1)
@@ -59,18 +68,18 @@ def run_sklearn_classification(trainingDatasetPath, PredictionDatasetPath, argsX
     predictedValues=list(clf.predict(mydata))
 
     #5. Save testing dataset with predictions
-    # file = open("prediction_sklearnClassifiaction.csv",'w')
-    # wr = csv.writer(file, dialect='excel')
-    # ds = [elem for elem in args_X]
-    # ds.append(argsY)
-    # ds.append("prediction")
-    # wr.writerow(ds)
-    # for i in range(len(predictedValues)):
-    #     rowcopy = [mydata[i][j] for j in range(len(args_X))]
-    #     rowcopy.append(ydata[i])
-    #     rowcopy.append(predictedValues[i])
-    #     wr.writerow(rowcopy)
-    # file.close()
+    file = open("prediction_sklearnClassifiaction.csv",'w')
+    wr = csv.writer(file, dialect='excel')
+    ds = [elem for elem in args_X]
+    ds.append(argsY)
+    ds.append("prediction")
+    wr.writerow(ds)
+    for i in range(len(predictedValues)):
+        rowcopy = [mydata[i][j] for j in range(len(args_X))]
+        rowcopy.append(ydata[i])
+        rowcopy.append(predictedValues[i])
+        wr.writerow(rowcopy)
+    file.close()
 
     # 6.Create confusion matrix
     confusionMatrix = dict()
@@ -134,26 +143,71 @@ def run_sklearn_regression(trainingDatasetPath, PredictionDatasetPath, argsX, ar
     predictedValues=list(regr.predict(mydata))
 
     #5. Save testing dataset with predictions
-    # file = open("prediction_sklearnRegression.csv",'w')
-    # wr = csv.writer(file, dialect='excel')
-    # ds = [elem for elem in args_X]
-    # ds.append(argsY)
-    # ds.append("prediction")
-    # wr.writerow(ds)
-    # for i in range(len(predictedValues)):
-    #     rowcopy = [mydata[i][j] for j in range(len(args_X))]
-    #     rowcopy.append(ydata[i])
-    #     rowcopy.append(predictedValues[i])
-    #     wr.writerow(rowcopy)
-    # file.close()
+    file = open("prediction_sklearnRegression.csv",'w')
+    wr = csv.writer(file, dialect='excel')
+    ds = [elem for elem in args_X]
+    ds.append(argsY)
+    ds.append("prediction")
+    wr.writerow(ds)
+    for i in range(len(predictedValues)):
+        rowcopy = [mydata[i][j] for j in range(len(args_X))]
+        rowcopy.append(ydata[i])
+        rowcopy.append(predictedValues[i])
+        wr.writerow(rowcopy)
+    file.close()
 
     #Model Evaluation
-    mse = mean_squared_error(ydata, predictedValues)
+    mse = metrics.mean_squared_error(ydata, predictedValues)
     rmse =np.sqrt(mse)
     return rmse
 
 class Test_Cart(unittest.TestCase):
     def test_Cart_1(self):
+        #Run Exareme Cart Training Algorithm
+        logging.info("---------- TEST 1: CART - classification tree on  diabetes")
+        data_Cart = [{ "name": "x", "value": "Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age"},
+                {"name": "y", "value": "Outcome"},
+                {"name": "max_depth", "value": "200"},
+                {"name": "no_split_points","value": "20"},
+                {"name": "pathology","value":"dementia"},
+                {"name": "dataset", "value": "diabetes_training"},
+                {"name": "filter", "value": ""}]
+        headers = {'Content-type': 'application/json', "Accept": "text/plain"}
+        r = requests.post(endpointUrl_CartTraining,data=json.dumps(data_Cart),headers=headers)
+        exaremeResult_Training = json.loads(r.text)
+
+        #########################'##########################################
+        #Run Exareme Cart Prediction Algorithm
+        data_CartPredict = [{ "name": "x", "value": data_Cart[0]["value"]},
+                {"name": "y", "value":  data_Cart[1]["value"]},
+                {"name": "treeJson", "value": ""},
+                {"name": "treeFile", "value": "tree.txt"},
+                {"name": "pathology","value":"dementia"},
+                {"name": "dataset", "value": "diabetes_testing"},
+                {"name": "filter", "value": ""}]
+        headers = {'Content-type': 'application/json', "Accept": "text/plain"}
+        r = requests.post(endpointUrl_CartPredict,data=json.dumps(data_CartPredict),headers=headers)
+        exaremeResult_Predict = json.loads(r.text)
+
+        ####################################################################
+        #Run Python
+        data_sklearn = {"trainingDataset": "diabetes_training",
+                        "testingDataset": "diabetes_testing",
+                        "x": "Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age",
+                        "y": "Outcome",
+                        "max_depth": 200}
+
+        sklearnconfusionMatrix = run_sklearn_classification( path+data_sklearn['trainingDataset']+".csv",
+                                                      path+data_sklearn['testingDataset']+".csv",
+                                                      data_sklearn['x'], data_sklearn['y'], data_sklearn['max_depth'])
+
+        ExaremeCorrectPredictions,ExaremeWrongPredictions,sklearnCorrectPredictions,sklearnWrongPredictions = evaluate_predictions(exaremeResult_Predict['result'][0]['data']['data'], sklearnconfusionMatrix)
+        print ("ConfusionMatrices(E/S):", r.text, sklearnconfusionMatrix)
+        print ("ExaremePredictions(C/W):",ExaremeCorrectPredictions,ExaremeWrongPredictions)
+        print ("sklearnPredictions(C/W):",sklearnCorrectPredictions,sklearnWrongPredictions)
+        assert (0)
+
+    def test_Cart_2(self):
         #Run Exareme Cart Training Algorithm
         logging.info("---------- TEST 1: CART - classification tree with real variables -  Training and prediction applied on the same dataset")
         data_Cart = [{ "name": "x", "value": "rightententorhinalarea, lefthippocampus, righthippocampus"},
@@ -187,6 +241,14 @@ class Test_Cart(unittest.TestCase):
                         "x": "rightententorhinalarea, lefthippocampus, righthippocampus",
                         "y": "alzheimerbroadcategory",
                         "max_depth": 200}
+
+        data_sklearn = {"trainingDataset": "adni",
+                        "testingDataset": "desd-synthdata",
+                        "x": "montrealcognitiveassessment,rightpcggposteriorcingulategyrus,leftpcggposteriorcingulategyrus,rightacgganteriorcingulategyrus,leftacgganteriorcingulategyrus,rightmcggmiddlecingulategyrus,leftmcggmiddlecingulategyrus,rightphgparahippocampalgyrus,leftphgparahippocampalgyrus,rightententorhinalarea,leftententorhinalarea,righthippocampus,lefthippocampus,rightthalamusproper,leftthalamusproper",
+                        "y": "alzheimerbroadcategory",
+                        "max_depth": 200}
+
+
         sklearnconfusionMatrix = run_sklearn_classification( path+data_sklearn['trainingDataset']+".csv",
                                                       path+data_sklearn['testingDataset']+".csv",
                                                       data_sklearn['x'], data_sklearn['y'], data_sklearn['max_depth'])
@@ -197,7 +259,7 @@ class Test_Cart(unittest.TestCase):
         print ("sklearnPredictions(C/W):",sklearnCorrectPredictions,sklearnWrongPredictions)
         assert (0)
 
-    def test_Cart_2(self):
+    def test_Cart_3(self):
         logging.info("---------- TEST 2: CART - regression tree with real variables -  Training and prediction applied on the same dataset")
         data_Cart = [{ "name": "x", "value": "rightententorhinalarea, lefthippocampus, righthippocampus"},
                 {"name": "y", "value": "subjectage"},
