5f1470ab1cf5a86b18c240422f87fa8015b19768
ezacharia <ezacharia@di.uoa.gr>
2020-06-05 17:54:17 +0300

    add Cart_Predict

---
+++ Exareme-Docker/src/mip-algorithms/CART_PREDICT/1/local.py
@@ -0,0 +1,67 @@
+from __future__ import division
+from __future__ import print_function
+
+import sys
+from os import path
+from argparse import ArgumentParser
+import sqlite3
+import json
+import pandas as pd
+import logging
+
+sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))) + '/utils/')
+sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))) + '/CART_PREDICT/')
+
+
+from algorithm_utils import query_database, variable_categorical_getDistinctValues, StateData, PrivacyError, ExaremeError, PRIVACY_MAGIC_NUMBER, init_logger
+from cartPredict_lib import cart_1_local, Cart_Loc2Glob_TD
+
+def main(args):
+    sys.argv =args
+    # Parse arguments
+    parser = ArgumentParser()
+    parser.add_argument('-x', required=True, help='Independent variable names, comma separated.')
+    parser.add_argument('-y', required=True, help='Dependent variable name')
+    parser.add_argument('-treeJson', required=True, help='Decision tree in json format')
+    parser.add_argument('-treeFile', required=True, help='Path of txt file that contains the decision tree in json format.')
+    parser.add_argument('-cur_state_pkl', required=True, help='Path to the pickle file holding the current state.')
+    parser.add_argument('-input_local_DB', required=True, help='Path to local db.')
+    parser.add_argument('-db_query', required=True, help='Query to be executed on local db.')
+    args, unknown = parser.parse_known_args()
+
+    #Get variable
+    args_X = list(args.x.replace(' ', '').split(','))
+    args_Y = [args.y.replace(' ', '')]
+
+    if args.treeJson is not '' and args.treeFile is '':
+        try:
+            args_globalTreeJ = json.loads(args.treeJson)
+        except:
+            raise ExaremeError('Tree json is too large. You should use treeFile input')
+    elif args.treeJson is '' and args.treeFile is not '':
+        file = open("/root/"+args.treeFile, "r")
+        args_globalTreeJ = json.loads(file.read())
+        file.close()
+    else:
+        raise ExaremeError('Only one of the fields treeJson and treeFile should be empty.')
+
+
+    init_logger()
+    logging.info(["Init1Local: args_X, args_Y",args_X, args_Y])
+
+    #1. Query database and metadata
+    queryMetadata = "select * from metadata where code in (" + "'" + "','".join(args_X) + "','" + "','".join(args_Y) + "'"  + ");"
+    dataSchema, metadataSchema, metadata, dataFrame  = query_database(fname_db=path.abspath(args.input_local_DB), queryData=args.db_query, queryMetadata=queryMetadata)
+    categoricalVariables = variable_categorical_getDistinctValues(metadata)
+
+    #2. Run algorithm
+    confusionMatrix, mse, counts, predictions = cart_1_local(dataFrame, dataSchema, categoricalVariables, args_X, args_Y, args_globalTreeJ)
+
+    #3. Save local state : Pass/
+
+    #4. Transfer local output
+    local_out = Cart_Loc2Glob_TD(args_X, args_Y, categoricalVariables, confusionMatrix, mse, counts, predictions)
+    local_out.transfer()
+
+if __name__ == '__main__':
+    main()
---
+++ Exareme-Docker/src/mip-algorithms/CART_PREDICT/cartPredict_lib.py
@@ -0,0 +1,165 @@
+
+from __future__ import division
+from __future__ import print_function
+
+import sys
+from os import path
+import pandas as pd
+import numpy as np
+import math
+import json
+import logging
+import itertools
+import csv
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))) + '/utils/')
+
+from algorithm_utils import TransferData, PRIVACY_MAGIC_NUMBER
+
+LOGS = True
+
+def totabulardataresourceformat(name, data, fields):
+    # Tabular data resource summary 2
+    result = {
+        "type": "application/vnd.dataresource+json",
+        "data":
+            {
+                "name"   : name,
+                "profile": "tabular-data-resource",
+                "data"   : data,
+                "schema" : {
+                    "fields": fields
+                }
+            }
+    }
+    return result
+
+def add_dict(dict1,dict2):
+    resultdict = dict()
+    for key in dict1:
+        if key in dict2:
+            resultdict [key] = dict1[key] + dict2[key]
+        else:
+            resultdict [key] = dict1[key]
+    for key in dict2:
+        if key not in dict1:
+            resultdict [key] = dict2[key]
+    return resultdict
+
+def add_vals(a,b):
+    if a == None and b == None:
+        return None
+    else:
+        return(a or 0 ) +( b or 0)
+
+def predict(node, row, args_Y, isClassificationTree):
+    if (isinstance(node['right'],dict) == False and isinstance(node['left'],dict) == False) or \
+       (isinstance(node['left'],dict) == False and row[node['colName']] <= node['threshold']) or \
+       (isinstance(node['right'],dict) == False and row[node['colName']] > node['threshold']):
+        if isClassificationTree:
+            return node['class']
+        else:
+            return node['classValue']
+    elif row[node['colName']] <= node['threshold']:
+        return predict(node['left'], row,args_Y,isClassificationTree)
+    else:
+        return predict(node['right'], row,args_Y,isClassificationTree)
+
+def cart_1_local(dataFrame, dataSchema, categoricalVariables, args_X, args_Y, globalTreeJ):
+    #1. Delete null values from DataFrame
+    dataFrame = dataFrame.dropna()
+    for x in dataSchema:
+        if x in categoricalVariables:
+            dataFrame = dataFrame[dataFrame[x].astype(bool)]
+
+    #2. Check privacy
+    counts = len(dataFrame)
+    if counts < PRIVACY_MAGIC_NUMBER:
+        raise PrivacyError('The Experiment could not run with the input provided because there are insufficient data.')
+
+    confusionMatrix = dict() # ConfusionMatrix['ActualValue', 'PredictedValue'] = ...
+    mse= 0 # mean square error
+
+    predictions = dict()
+    predictions["ids"] = []
+    predictions["predictions"] = []
+    if args_Y[0] in categoricalVariables:  #case of Classification tree
+        for element in itertools.product(categoricalVariables[args_Y[0]],categoricalVariables[args_Y[0]]):
+            confusionMatrix[element[0],element[1]] = 0
+        for index, row in dataFrame.iterrows():
+            predictedValue = predict(globalTreeJ, row, args_Y, True)
+            if "IdForTesting" in dataSchema:
+                predictions["ids"].append(row["IdForTesting"])
+                predictions["predictions"].append(predictedValue)
+            logging.debug(["Predictions", row, predictedValue])
+            confusionMatrix[row[args_Y[0]],predictedValue] = confusionMatrix[row[args_Y[0]],predictedValue] + 1
+    elif args_Y[0] not in categoricalVariables: #case of regression tree
+        for index, row in dataFrame.iterrows():
+            predictedValue = predict(globalTreeJ, row, args_Y, False)
+            if "IdForTesting" in dataSchema:
+                predictions["ids"].append(row["IdForTesting"])
+                predictions["predictions"].append(predictedValue)
+
+            logging.debug(["Predictions", row, predictedValue])
+            mse = mse + (row[args_Y[0]] - predictedValue) ** 2
+
+
+    return confusionMatrix, mse, counts, predictions
+
+
+def cart_1_global(args_X, args_Y, categoricalVariables, confusionMatrix, mse, counts, predictions):
+    if args_Y[0] in categoricalVariables:
+        fields = [{"name": "Actual Value", "type": "text"},{"name": "Predicted Value", "type": "text"},{"name": "Counts", "type": "number"}]
+        table = totabulardataresourceformat("Confusion Matrix", [[key[0], key[1], confusionMatrix[key]] for key in confusionMatrix], fields)
+        predictions = {
+            "type": "application/json",
+            "data":  predictions
+        }
+        result = {"result": [ table, predictions ]}
+
+    if args_Y[0] not in categoricalVariables:  #case of Classification tree
+        mse = np.sqrt(mse / counts)
+        fields = [{"name": "mse", "type": "number"}]
+        table = totabulardataresourceformat("Mean Square Error", [mse] , fields)
+        predictions = {
+            "type": "application/json",
+            "data":  predictions
+        }
+        result = {"result": [ table, predictions ]}
+    try:
+        global_out = json.dumps(result, allow_nan=False)
+    except ValueError:
+        raise ValueError('Result contains NaNs.')
+    return global_out
+
+
+class Cart_Loc2Glob_TD(TransferData):
+    def __init__(self, *args):
+        if len(args) != 7:
+            raise ValueError('Illegal number of arguments.')
+        self.args_X = args[0]
+        self.args_Y = args[1]
+        self.categoricalVariables = args[2]
+        self.confusionMatrix = args[3]
+        self.mse = args[4]
+        self.counts = args[5]
+        self.predictions = args[6]
+
+    def get_data(self):
+        return self.args_X, self.args_Y, self.categoricalVariables, self.confusionMatrix, self.mse, self.counts, self.predictions
+
+    def __add__(self, other):
+
+        predictionsTotal = dict()
+        predictionsTotal["ids"] = self.predictions["ids"] + other.predictions["ids"]
+        predictionsTotal["predictions"] = self.predictions["predictions"] + other.predictions["predictions"]
+        
+        return Cart_Loc2Glob_TD(
+            self.args_X,
+            self.args_Y,
+            self.categoricalVariables,
+            add_dict(self.confusionMatrix,other.confusionMatrix),
+            add_vals(self.mse, other.mse),
+            add_vals(self.counts, other.counts),
+            predictionsTotal
+        )
