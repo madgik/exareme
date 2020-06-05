5d99f5e9d0f52e85ee08f2c742b618bb29bd9ee6
ezacharia <ezacharia@di.uoa.gr>
2020-06-05 18:57:24 +0300

    remove: save tree to file

--- Exareme-Docker/src/mip-algorithms/CART/finalize/1/local.py
+++ Exareme-Docker/src/mip-algorithms/CART/finalize/1/local.py
@@ -13,22 +13,23 @@ sys.path.append(path.dirname(path.dirname(path.dirname(path.dirname(path.abspath
 from cart_lib import Cart_Glob2Loc_TD,Node
 
 def main(args):
-    sys.argv =args
-    # Parse arguments
-    parser = ArgumentParser()
-    parser.add_argument('-cur_state_pkl', required=True, help='Path to the pickle file holding the current state.')
-    parser.add_argument('-prev_state_pkl', required=True, help='Path to the pickle file holding the previous state.')
-    parser.add_argument('-global_step_db', required=True, help='Path to db holding global step results.')
-    args, unknown = parser.parse_known_args()
-
-    # Load global node output
-    globalTree, activePaths = Cart_Glob2Loc_TD.load(path.abspath(args.global_step_db)).get_data()
-
-    # Save output to txt file
-    globalTreeJ = globalTree.tree_to_json()
-    file = open("/root/tree.txt","w")
-    file.write(json.dumps(globalTreeJ))
-    file.close()
+    pass
+    # sys.argv =args
+    # # Parse arguments
+    # parser = ArgumentParser()
+    # parser.add_argument('-cur_state_pkl', required=True, help='Path to the pickle file holding the current state.')
+    # parser.add_argument('-prev_state_pkl', required=True, help='Path to the pickle file holding the previous state.')
+    # parser.add_argument('-global_step_db', required=True, help='Path to db holding global step results.')
+    # args, unknown = parser.parse_known_args()
+    #
+    # # Load global node output
+    # globalTree, activePaths = Cart_Glob2Loc_TD.load(path.abspath(args.global_step_db)).get_data()
+    #
+    # # Save output to txt file
+    # globalTreeJ = globalTree.tree_to_json()
+    # file = open("/root/tree.txt","w")
+    # file.write(json.dumps(globalTreeJ))
+    # file.close()
 
 if __name__ == '__main__':
     main()
