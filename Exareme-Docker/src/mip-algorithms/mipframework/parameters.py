fd22f67c769651f3e23b0b75b64d34367353070f
Jason Sakellariou <js.sakel@gmail.com>
2020-11-11 23:19:29 +0200

    Correctly escape param values starting with a dash

--- Exareme-Docker/src/mip-algorithms/mipframework/parameters.py
+++ Exareme-Docker/src/mip-algorithms/mipframework/parameters.py
@@ -42,16 +42,28 @@ class Parameters(object):
 def parse_exareme_args(fp, cli_args):
     parser = ArgumentParser()
     # Add common arguments
-    for arg in COMMON_ALGORITHM_ARGUMENTS:
-        parser.add_argument("-" + arg)
+    for argname in COMMON_ALGORITHM_ARGUMENTS:
+        parser.add_argument("-" + argname)
     # Add algorithm specific arguments
     prop_path = os.path.join(fp, "properties.json")
     with open(prop_path, "r") as prop:
         params = json.load(prop)["parameters"]
+    algorithm_param_names = []
     for p in params:
         name = "-" + p["name"]
+        algorithm_param_names.append(p["name"])
         required = p["valueNotBlank"]
         parser.add_argument(name, required=required)
+    # Escape args starting with dash (e.g. see agegroup='-50y')
+    all_args = set(COMMON_ALGORITHM_ARGUMENTS) | set(algorithm_param_names)
+    escaped_args = []  # remember escaped args to undo later (see below)
+    for i, argname in enumerate(cli_args):
+        if argname.replace("-", "") in all_args:
+            continue
+        if argname.startswith("-"):
+            cli_args[i] = "\\" + argname
+            # arg name is one position begore its value
+            escaped_args.append(cli_args[i - 1].replace("-", ""))
     # Parse and process
     args, _ = parser.parse_known_args(cli_args)
     args.y = re.split(r"\s*,\s*", args.y)
@@ -65,4 +77,9 @@ def parse_exareme_args(fp, cli_args):
     args.filter = json.loads(args.filter) if args.filter else None
     if hasattr(args, "coding"):
         args.coding = None if args.coding == "null" else args.coding
+    # Undo arg escaping (see above)
+    if escaped_args:
+        for argname in escaped_args:
+            argval_without_escape = getattr(args, argname).replace("\\", "")
+            setattr(args, argname, argval_without_escape)
     return args
