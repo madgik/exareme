ab9c645ab5a4c41b3044a3e808a544bcb0114bee
Jason Sakellariou <js.sakel@gmail.com>
2021-01-06 18:37:28 +0200

    Add a AdditiveMulticlassROCCurve class
    This class, like all other Additive... classes,
    takes care of correctly computing the quantity in question, here
    the ROC curve, when multiple instances are sent from the locals to
    the global node.

--- Exareme-Docker/src/mip-algorithms/mipframework/funclib/crossvalidation.py
+++ Exareme-Docker/src/mip-algorithms/mipframework/funclib/crossvalidation.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+
 from collections import namedtuple
 
 import numpy as np
@@ -87,3 +91,83 @@ def compute_roc(true_positives, true_negatives, false_positives, false_negatives
     auc = np.trapz(tp_rate, fp_rate)
     gini = 2 * auc - 1
     return roc_curve, auc, gini
+
+
+class AdditiveMulticlassROCCurve(object):
+    def __init__(
+        self,
+        y_true=None,
+        y_pred_proba_per_class=None,
+        classes=None,
+        tp=None,
+        tn=None,
+        fp=None,
+        fn=None,
+    ):
+        if (tp, tn, fp, fn) == (None, None, None, None):
+            if len(y_true.shape) > 1:
+                y_true = y_true.flatten()
+            self.tp = []
+            self.tn = []
+            self.fp = []
+            self.fn = []
+            self.classes = classes
+            for ci, c in enumerate(classes):
+                y_pred_proba = y_pred_proba_per_class[:, ci]
+                thres = np.linspace(1.0, 0.0, num=2 ** 7 + 1)
+                self.tp.append(
+                    ((y_true == c) & (y_pred_proba >= thres[:, None])).sum(axis=1)
+                )
+                self.tn.append(
+                    ((y_true != c) & (y_pred_proba < thres[:, None])).sum(axis=1)
+                )
+                self.fp.append(
+                    ((y_true != c) & (y_pred_proba >= thres[:, None])).sum(axis=1)
+                )
+                self.fn.append(
+                    ((y_true == c) & (y_pred_proba < thres[:, None])).sum(axis=1)
+                )
+        elif tp and tn and fp and fn:
+            self.tp = tp
+            self.tn = tn
+            self.fp = fp
+            self.fn = fn
+
+    def __add__(self, other):
+        result = AdditiveMulticlassROCCurve(
+            tp=[tp_1 + tp_2 for tp_1, tp_2 in zip(self.tp, other.tp)],
+            tn=[tn_1 + tn_2 for tn_1, tn_2 in zip(self.tn, other.tn)],
+            fp=[fp_1 + fp_2 for fp_1, fp_2 in zip(self.fp, other.fp)],
+            fn=[fn_1 + fn_2 for fn_1, fn_2 in zip(self.fn, other.fn)],
+        )
+        if (self.classes == other.classes).all():
+            result.classes = self.classes
+        else:
+            raise ValueError("classes do not agree")
+        return result
+
+    def get_curves(self):
+        curves = []
+        for ci, c in enumerate(self.classes):
+            tpr = self.tp[ci] / (self.tp[ci] + self.fn[ci])
+            tpr[np.isnan(tpr)] = 1.0
+            fpr = self.fp[ci] / (self.fp[ci] + self.tn[ci])
+            fpr[np.isnan(fpr)] = 1.0
+            curves.append((fpr.tolist(), tpr.tolist()))
+        return curves
+
+
+if __name__ == "__main__":
+    classes = np.array(["AD", "CN", "Other"])
+    y_true = np.array(["AD", "AD", "Other", "CN", "CN", "AD"])
+    y_pred_proba_per_class = np.array(
+        [
+            [0.6, 0.2, 0.1],
+            [0.38, 0.42, 0.2],
+            [0.4, 0.1, 0.5],
+            [0.3, 0.3, 0.4],
+            [0.2, 0.45, 0.35],
+            [0.5, 0.3, 0.2],
+        ]
+    )
+    roc = MulticlassROCCurve(y_true, y_pred_proba_per_class, classes)
