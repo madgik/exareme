af2242c2d91cb60cbd167b03f6d3fe25a3602018
Jason Sakellariou <js.sakel@gmail.com>
2021-01-14 11:33:52 +0200

    One-way anova (signle covariable) with result table

--- Exareme-Docker/src/mip-algorithms/ANOVA_PY/anova2.py
+++ Exareme-Docker/src/mip-algorithms/ANOVA_PY/anova2.py
@@ -2,12 +2,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import numpy as np
 import pandas as pd
 import scipy.stats
 
 from mipframework import Algorithm
 from mipframework import AlgorithmResult
+from mipframework import TabularDataResource
 
 
 class Anova(Algorithm):
@@ -17,75 +17,82 @@ class Anova(Algorithm):
     def local_(self):
         X = self.data.full
         variable = self.parameters.y[0]
-        covariables = self.parameters.x
-        model = AdditiveAnoveModel(X, variable, covariables)
+        covariable = self.parameters.x[0]
+        covar_label = self.metadata.label[covariable]
+
+        model = AdditiveAnovaModel(X, variable, covariable)
 
         self.push_and_add(model=model)
+        self.push_and_agree(covar_label=covar_label)
 
     def global_(self):
         model = self.fetch("model")
+        covar_label = self.fetch("covar_label")
 
-        df_explained = model.get_df_explained()
-        df_residual = model.get_df_residual(df_explained)
+        res = model.get_anova_table()
 
-        ss_residual = model.get_ss_residual()
-        ss_explained = model.get_ss_explained(model.covariables[0])
-
-        table = model.get_anova_table()
+        table = TabularDataResource(
+            fields=["", "df", "sum_sq", "mean_sq", "F", "Pr(>F)"],
+            data=[
+                [
+                    covar_label,
+                    res["df_explained"],
+                    res["ss_explained"],
+                    res["ms_explained"],
+                    res["f_stat"],
+                    res["p_value"],
+                ],
+                [
+                    "Residual",
+                    res["df_residual"],
+                    res["ss_residual"],
+                    res["ms_residual"],
+                    None,
+                    None,
+                ],
+            ],
+            title="Anova Summary",
+        )
 
         self.result = AlgorithmResult(
-            raw_data={
-                "model": model.to_dict(),
-                "df_explained": df_explained,
-                "df_residual": df_residual,
-                "ss_residual": ss_residual,
-                "ss_explained": ss_explained,
-                "table": table,
-            },
-            tables=[],
-            highcharts=[],
+            raw_data={"anova_table": res,}, tables=[table], highcharts=[],
         )
 
 
-class AdditiveAnoveModel(object):
-    def __init__(self, X=None, variable=None, covariables=None):
-        if X is not None and variable and covariables:
-            var_sq = variable + "_sq"
-            X[var_sq] = X[variable] ** 2
-
+class AdditiveAnovaModel(object):
+    def __init__(self, X=None, variable=None, covariable=None):
+        if X is not None and variable and covariable:
             self.variable = variable
-            self.covariables = covariables
+            self.covariable = covariable
+            self.var_sq = variable + "_sq"
+            X[self.var_sq] = X[variable] ** 2
 
             self.n_obs = X.shape[0]
 
-            self.overall_stats = self.get_overall_stats(X, variable, var_sq)
+            self.overall_stats = self.get_overall_stats(X)
 
-            self.group_stats = self.get_group_stats(X, variable, var_sq, covariables)
+            self.group_stats = self.get_group_stats(X)
 
     def __add__(self, other):
-        result = AdditiveAnoveModel()
+        result = AdditiveAnovaModel()
 
         assert self.variable == other.variable, "variable names do not agree"
         result.variable = self.variable
 
-        assert self.covariables == other.covariables, "covariables names do not agree"
-        result.covariables = self.covariables
+        assert self.covariable == other.covariable, "covariable names do not agree"
+        result.covariable = self.covariable
 
         result.n_obs = self.n_obs + other.n_obs
 
         result.overall_stats = self.overall_stats + other.overall_stats
 
-        result.group_stats = {
-            k: v1.add(v2, fill_value=0)
-            for (k, v1), (_, v2) in zip(
-                self.group_stats.items(), other.group_stats.items()
-            )
-        }
+        result.group_stats = self.group_stats.add(other.group_stats, fill_value=0)
 
         return result
 
-    @staticmethod
-    def get_overall_stats(X, variable, var_sq):
+    def get_overall_stats(self, X):
+        variable = self.variable
+        var_sq = self.var_sq
         overall_stats = X[variable].agg(["count", "sum"])
         overall_ssq = X[var_sq].sum()
         overall_stats = overall_stats.append(
@@ -93,55 +100,45 @@ class AdditiveAnoveModel(object):
         )
         return overall_stats
 
-    @staticmethod
-    def get_group_stats(X, variable, var_sq, covariables):
-        group_stats = dict()
-        for cov in covariables:
-            group_stat = X[[variable, cov]].groupby(cov).agg(["count", "sum"])
-            group_stat.columns = ["count", "sum"]
-            group_sum_sq = X[[var_sq, cov]].groupby(cov).agg(["sum"])
-            group_sum_sq.columns = ["sum_sq"]
-            group_stats[cov] = group_stat.join(group_sum_sq)
+    def get_group_stats(self, X):
+        variable = self.variable
+        covar = self.covariable
+        group_stats = X[[variable, covar]].groupby(covar).agg(["count", "sum"])
+        group_stats.columns = ["count", "sum"]
         return group_stats
 
     def get_df_explained(self):
-        return {cov: len(group) - 1 for cov, group in self.group_stats.items()}
+        return len(self.group_stats) - 1
 
-    def get_df_residual(self, df_explained):
-        return {cov: self.n_obs - dfe for cov, dfe in df_explained.items()}
+    def get_df_residual(self):
+        return self.n_obs - len(self.group_stats)
 
     def get_ss_residual(self):
-        ss_residual = dict()
-        for cov, group in self.group_stats.items():
-            ssr = self.overall_stats["sum_sq"] - sum(group["sum"] ** 2 / group["count"])
-            ss_residual[cov] = ssr
-        return ss_residual
+        overall_sum_sq = self.overall_stats["sum_sq"]
+        group_sum = self.group_stats["sum"]
+        group_count = self.group_stats["count"]
+        return overall_sum_sq - sum(group_sum ** 2 / group_count)
 
     def get_ss_total(self):
-        return self.overall_stats["sum_sq"] - (
-            self.overall_stats["sum"] ** 2 / self.overall_stats["count"]
-        )
+        overall_sum_sq = self.overall_stats["sum_sq"]
+        overall_sum = self.overall_stats["sum"]
+        overall_count = self.overall_stats["count"]
+        return overall_sum_sq - (overall_sum ** 2 / overall_count)
 
-    def get_ss_explained(self, covariable):
-        return sum(
-            (
-                self.overall_mean
-                - self.group_stats[covariable]["sum"]
-                / self.group_stats[covariable]["count"]
-            )
-            ** 2
-            * self.group_stats[covariable]["count"]
-        )
+    def get_ss_explained(self):
+        group_sum = self.group_stats["sum"]
+        group_count = self.group_stats["count"]
+        return sum((self.overall_mean - group_sum / group_count) ** 2 * group_count)
 
     def get_anova_table(self):
         df_explained = self.get_df_explained()
-        df_residual = self.get_df_residual(df_explained)
-        ss_explained = self.get_ss_explained(self.covariables[0])
+        df_residual = self.get_df_residual()
+        ss_explained = self.get_ss_explained()
         ss_residual = self.get_ss_residual()
         ms_explained = ss_explained / df_explained
         ms_residual = ss_residual / df_residual
         f_stat = ms_explained / ms_residual
-        p_value = scipy.stats.f.cdf(f_stat, df_explained, df_residual)
+        p_value = 1 - scipy.stats.f.cdf(f_stat, df_explained, df_residual)
         return dict(
             df_explained=df_explained,
             df_residual=df_residual,
@@ -160,10 +157,10 @@ class AdditiveAnoveModel(object):
     def to_dict(self):  # useful for debugging
         dd = {
             "variable": self.variable,
-            "covariables": self.covariables,
+            "covariable": self.covariable,
             "n_obs": self.n_obs,
             "overall_stats": self.overall_stats.tolist(),
-            "group_stats": {k: v.values.tolist() for k, v in self.group_stats.items()},
+            "group_stats": self.group_stats.values.tolist(),
         }
         return dd
 
@@ -176,11 +173,11 @@ if __name__ == "__main__":
         "-y",
         "lefthippocampus",
         "-x",
-        "alzheimerbroadcategory",
+        "ppmicategory",
         "-pathology",
         "dementia",
         "-dataset",
-        "edsd",
+        "ppmi",
         "-filter",
         "",
     ]
