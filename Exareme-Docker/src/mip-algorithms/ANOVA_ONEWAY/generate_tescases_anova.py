f0fcdf69be461a1aba12a8063c8d0c848eb82e6c
Jason Sakellariou <js.sakel@gmail.com>
2021-01-17 13:04:58 +0200

    Add Tukey results to Anova algorithm tests

--- Exareme-Docker/src/mip-algorithms/ANOVA_ONEWAY/generate_tescases_anova.py
+++ Exareme-Docker/src/mip-algorithms/ANOVA_ONEWAY/generate_tescases_anova.py
@@ -1,7 +1,13 @@
+# XXX this module runs on python 3.9 because we need pingouin to calculate
+# Tukey table.
+
 from pathlib import Path
 
+import numpy as np
+from pingouin import pairwise_tukey
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
+from statsmodels.stats.libqsturng import psturng
 
 from mipframework.algorithmtest import AlgorithmTest
 from mipframework.exceptions import PrivacyError
@@ -23,6 +29,7 @@ class AnovaTest(AlgorithmTest):
             return None
         data = data.dropna()
         n_obs = len(data)
+        n_groups = len(set(data[x_names]))
 
         # If n_obs < n_cols reject
         if n_obs <= PRIVACY_MAGIC_NUMBER:
@@ -30,13 +37,31 @@ class AnovaTest(AlgorithmTest):
         if data[x_names].unique().shape[0] < 2:
             return None
 
+        # Anova
         formula = "{y} ~ {x}".format(y=y_names, x=x_names)
         lm = ols(formula, data=data).fit()
         aov = sm.stats.anova_lm(lm)
-
         result = aov.to_dict()
-        # del result['F']['Residual']
-        # del result['PR(>F)']['Residual']
+
+        # # Tukey test
+        tukey = pairwise_tukey(data=data, dv=y_names, between=x_names)
+        tukey_results = []
+        for _, row in tukey.iterrows():
+            tukey_result = dict()
+            tukey_result["groupA"] = row["A"]
+            tukey_result["groupB"] = row["B"]
+            tukey_result["meanA"] = row["mean(A)"]
+            tukey_result["meanB"] = row["mean(B)"]
+            tukey_result["diff"] = row["diff"]
+            tukey_result["se"] = row["se"]
+            tukey_result["t_stat"] = row["T"]
+            # computing pval because pingouin and statsmodels implementations
+            # of pstrung do not agree
+            pval = psturng(
+                np.sqrt(2) * np.abs(row["T"]), n_groups, result["df"]["Residual"]
+            )
+            tukey_result["p_tukey"] = float(pval)
+            tukey_results.append(tukey_result)
 
         expected_out = dict()
         expected_out["df_residual"] = result["df"]["Residual"]
@@ -47,6 +72,7 @@ class AnovaTest(AlgorithmTest):
         expected_out["ms_explained"] = result["mean_sq"][x_names]
         expected_out["p_value"] = result["PR(>F)"][x_names]
         expected_out["f_stat"] = result["F"][x_names]
+        expected_out["tukey_test"] = tukey_results
 
         return expected_out
 
--- Exareme-Docker/src/mip-algorithms/tests/algorithm_tests/test_anova.py
+++ Exareme-Docker/src/mip-algorithms/tests/algorithm_tests/test_anova.py
@@ -12,19 +12,35 @@ expected_file = Path(__file__).parent / "expected" / "anova_expected.json"
     "test_input, expected", get_test_params(expected_file, slice(80))
 )
 def test_anova_algorithm_local(test_input, expected):
-    result = get_algorithm_result(Anova, test_input, num_workers=1)["anova_table"]
-    assert set(expected.keys()) == set(result.keys())
-    for key, e_val in expected.items():
-        r_val = result[key]
+    result = get_algorithm_result(Anova, test_input, num_workers=1)
+    aov = result["anova_table"]
+    tukey = result["tukey_table"]
+    e_aov = {k: v for k, v in expected.items() if k != "tukey_test"}
+    e_tukey = expected["tukey_test"]
+    assert set(e_aov) == set(aov.keys())
+    for key, e_val in e_aov.items():
+        r_val = aov[key]
         assert np.isclose(e_val, r_val)
+    for et, rt in zip(e_tukey, tukey):
+        for key, e_val in et.items():
+            r_val = rt[key]
+            assert e_val == r_val or np.isclose(e_val, r_val)
 
 
 @pytest.mark.parametrize(
     "test_input, expected", get_test_params(expected_file, slice(80, 95))
 )
 def test_anova_algorithm_federated(test_input, expected):
-    result = get_algorithm_result(Anova, test_input, num_workers=10)["anova_table"]
-    assert set(expected.keys()) == set(result.keys())
-    for key, e_val in expected.items():
-        r_val = result[key]
+    result = get_algorithm_result(Anova, test_input, num_workers=10)
+    aov = result["anova_table"]
+    tukey = result["tukey_table"]
+    e_aov = {k: v for k, v in expected.items() if k != "tukey_test"}
+    e_tukey = expected["tukey_test"]
+    assert set(e_aov) == set(aov.keys())
+    for key, e_val in e_aov.items():
+        r_val = aov[key]
         assert np.isclose(e_val, r_val)
+    for et, rt in zip(e_tukey, tukey):
+        for key, e_val in et.items():
+            r_val = rt[key]
+            assert e_val == r_val or np.isclose(e_val, r_val)
