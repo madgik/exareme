Formula refactoring and bug fixes

Exareme-Docker/src/mip-algorithms/mipframework/data.py
@@ -8,10 +8,12 @@ from sqlalchemy import between, not_, and_, or_, Table, select, create_engine, M
 from mipframework.constants import PRIVACY_THRESHOLD
 from mipframework.loggingutils import log_this, repr_with_logging, logged
 from mipframework.exceptions import PrivacyError
-from mipframework.formula import generate_formula
+from mipframework.formula import (
+    generate_formula,
+    generate_formula_from_variable_lists,
+    insert_explicit_coding_for_categorical_vars,
+)
 
-
-# TODO remove formula_is_equation flag and always prepend 'y ~ ' to formula
 
 FILTER_OPERATORS = {
     "equal": lambda a, b: a == b,
@@ -42,8 +44,22 @@ class AlgorithmData(object):
         self.db = db
         self.full = db.read_data_from_db(args)
         self.metadata = db.read_metadata_from_db(args)
-        variables, covariables = self.build_variables(args)
-        if 1 in self.metadata.is_categorical.values():
+        if hasattr(args, "formula") and args.formula:
+            formula = generate_formula(args.formula, args.y[0])
+        else:
+            formula = generate_formula_from_variable_lists(args)
+        if self.some_vars_are_categorical():
+            formula = insert_explicit_coding_for_categorical_vars(
+                formula,
+                args,
+                self.metadata.is_categorical,
+            )
+        variables, covariables = self.build_variables_from_formula(
+            formula,
+            args,
+            full_data_table=self.full,
+        )
+        if self.some_vars_are_categorical():
             variables = self.add_missing_levels(args.y, args.coding, variables)
             if covariables is not None:  # truth value of dataframe is ambiguous
                 covariables = self.add_missing_levels(args.x, args.coding, covariables)
@@ -52,27 +68,22 @@ class AlgorithmData(object):
     def __repr__(self):
         repr_with_logging(self, variables=self.variables, covariables=self.covariables)
 
-    def build_variables(self, args):
-        log_this("AlgorithmData.build_variables", args=args)
+    def some_vars_are_categorical(self):
+        return 1 in self.metadata.is_categorical.values()
 
-        from numpy import log as log
-        from numpy import exp as exp
+    def build_variables_from_formula(self, formula, args, full_data_table):
+        log_this("AlgorithmData.build_variables_from_formula", args=args)
 
-        formula = self.get_formula(args)
-        # FIXME: hack, should remove formula_is_equation flag and always prepend 'y ~ ' to formula
-        args.formula_is_equation = True
-        formula = args.y[0] + " ~ " + formula
-        # end of hack
-        if args.formula_is_equation:
-            if self.full.dropna().shape[0] == 0:
+        if "~" in formula:
+            if full_data_table.dropna().shape[0] == 0:
                 return pd.DataFrame(), pd.DataFrame()
             variables, covariables = patsy.dmatrices(
-                formula, self.full, return_type="dataframe"
+                formula, full_data_table, return_type="dataframe"
             )
         else:
-            if self.full.dropna().shape[0] == 0:
+            if full_data_table.dropna().shape[0] == 0:
                 return pd.DataFrame(), None
-            variables = patsy.dmatrix(formula, self.full, return_type="dataframe")
+            variables = patsy.dmatrix(formula, full_data_table, return_type="dataframe")
             covariables = None
         return variables, covariables
 
@@ -97,35 +108,6 @@ class AlgorithmData(object):
             missing_column = pd.Series(np.zeros((len(dmatrix),)), index=dmatrix.index)
             dmatrix[var_level] = missing_column
         return dmatrix
-
-    def get_formula(self, args):
-        log_this("AlgorithmData.get_formula", args=args)
-        is_categorical = self.metadata.is_categorical
-        # Get formula from args or build if doesn't exist
-        if hasattr(args, "formula") and args.formula:
-            formula = generate_formula(args.formula)
-        else:
-            if hasattr(args, "x") and args.x:
-                formula = "+".join(args.y) + "~" + "+".join(args.x)
-                if not args.intercept:
-                    formula += "-1"
-            else:
-                formula = "+".join(args.y) + "-1"
-        # Process categorical vars
-        var_names = list(args.y)
-        if hasattr(args, "x") and args.x:
-            var_names.extend(args.x)
-        if 1 in is_categorical.values():
-            if not hasattr(args, "coding") or not args.coding:
-                args.coding = "Treatment"
-            for var in var_names:
-                if is_categorical[var]:
-                    formula = re.sub(
-                        r"\b({})\b".format(var),
-                        r"C(\g<0>, {})".format(args.coding),
-                        formula,
-                    )
-        return formula
 
 
 class AlgorithmMetadata(object):

Exareme-Docker/src/mip-algorithms/mipframework/formula.py
@@ -1,4 +1,7 @@
-def generate_formula(formula_data):
+import re
+
+
+def generate_formula(formula_data, dependent_var=""):
     single_terms = []
     for term in formula_data["single"]:
         var_name = term["var_name"]
@@ -17,15 +20,27 @@ def generate_formula(formula_data):
     formula_expr = " + ".join(single_terms)
     if interactions:
         formula_expr += " + " + " + ".join(interactions)
+    if dependent_var:
+        return dependent_var + "~" + formula_expr
     return formula_expr
 
 
 def get_term_unary_op(var_name, op):
-    if op not in ("nop", "log", "exp", "center", "standardize"):
-        raise FormulaInvalidOperator("Invalid operator: {}".format(op))
-    if op == "nop":
-        return var_name
-    return "{0}({1})".format(op, var_name)
+    op_to_formula_term = {
+        "nop": "{}",
+        "log": "np.log({})",
+        "exp": "np.exp({})",
+        "center": "patsy.center({})",
+        "standardize": "patsy.standardize({})",
+        "dummy": "C({}, Treatment)",
+        "diff": "C({}, Diff)",
+        "poly": "C({}, Poly)",
+        "sum": "C({}, Sum)",
+        "Helmert": "C({}, Helmert)",
+    }
+    if op in op_to_formula_term.keys():
+        return op_to_formula_term[op].format(var_name)
+    raise FormulaInvalidOperator("Invalid operator: {}".format(op))
 
 
 def get_term_binary_op(var_name, op, operand):
@@ -38,3 +53,26 @@ def get_term_binary_op(var_name, op, operand):
 
 class FormulaInvalidOperator(Exception):
     """Raise when an invalid operator is encountered."""
+
+
+def generate_formula_from_variable_lists(args):
+    if hasattr(args, "x") and args.x:
+        formula = "+".join(args.y) + "~" + "+".join(args.x)
+        if not args.intercept:
+            formula += "-1"
+    else:
+        formula = "+".join(args.y) + "-1"
+    return formula
+
+
+def insert_explicit_coding_for_categorical_vars(formula, args, is_categorical):
+    if not hasattr(args, "coding") or not args.coding:
+        args.coding = "Treatment"
+    for var in args.var_names:
+        if is_categorical[var]:
+            formula = re.sub(
+                r"\b({})\b".format(var),
+                r"C(\g<0>, {})".format(args.coding),
+                formula,
+            )
+    return formula

Exareme-Docker/src/mip-algorithms/mipframework/tests/test_formula.py
@@ -5,7 +5,17 @@ from mipframework.formula import (
     get_term_unary_op,
     FormulaInvalidOperator,
     get_term_binary_op,
+    generate_formula_from_variable_lists,
+    insert_explicit_coding_for_categorical_vars,
 )
+
+
+@pytest.fixture
+def args():
+    class Args:
+        pass
+
+    return Args()
 
 
 def test_unparse_formula_one_single_nop():
@@ -18,12 +28,22 @@ def test_unparse_formula_one_single_nop():
     assert expected == result
 
 
+def test_generate_formula_with_dependent_var():
+    formula_data = {
+        "single": [{"var_name": "var1", "unary_operation": "nop"}],
+        "interactions": [],
+    }
+    expected = "y~var1"
+    result = generate_formula(formula_data, "y")
+    assert expected == result
+
+
 def test_unparse_formula_one_single_log():
     formula_data = {
         "single": [{"var_name": "var1", "unary_operation": "log"}],
         "interactions": [],
     }
-    expected = "log(var1)"
+    expected = "np.log(var1)"
     result = generate_formula(formula_data)
     assert expected == result
 
@@ -36,7 +56,7 @@ def test_unparse_formula_two_singles():
         ],
         "interactions": [],
     }
-    expected = "var1 + log(var2)"
+    expected = "var1 + np.log(var2)"
     result = generate_formula(formula_data)
     assert expected == result
 
@@ -65,7 +85,7 @@ def test_unparse_formula_multiple_singles():
         ],
         "interactions": [],
     }
-    expected = "var1 + log(var2) + center(var3) + 2*var4 + var5/2"
+    expected = "var1 + np.log(var2) + patsy.center(var3) + 2*var4 + var5/2"
     result = generate_formula(formula_data)
     assert expected == result
 
@@ -111,15 +131,71 @@ def test_unparse_formula_one_triple_interaction():
             {"var1": "var1", "var2": "var2", "var3": "var3"},
         ],
     }
-    expected = "var1 + var2 + var3 + var1:var2:var3"
+    expected = "var1 + var2 + var3 + var1:var3:var2"
     result = generate_formula(formula_data)
     assert expected == result
 
 
-def test_get_term_unary_op():
+def test_get_term_unary_op_nop():
     var_name = "var1"
     op = "nop"
     expected = "var1"
+    result = get_term_unary_op(var_name, op)
+    assert expected == result
+
+
+def test_get_term_unary_op_log():
+    var_name = "var1"
+    op = "log"
+    expected = "np.log(var1)"
+    result = get_term_unary_op(var_name, op)
+    assert expected == result
+
+
+def test_get_term_unary_op_exp():
+    var_name = "var1"
+    op = "exp"
+    expected = "np.exp(var1)"
+    result = get_term_unary_op(var_name, op)
+    assert expected == result
+
+
+def test_get_term_unary_op_center():
+    var_name = "var1"
+    op = "center"
+    expected = "patsy.center(var1)"
+    result = get_term_unary_op(var_name, op)
+    assert expected == result
+
+
+def test_get_term_unary_op_standardize():
+    var_name = "var1"
+    op = "standardize"
+    expected = "patsy.standardize(var1)"
+    result = get_term_unary_op(var_name, op)
+    assert expected == result
+
+
+def test_get_term_unary_op_dummy():
+    var_name = "var1"
+    op = "dummy"
+    expected = "C(var1, Treatment)"
+    result = get_term_unary_op(var_name, op)
+    assert expected == result
+
+
+def test_get_term_unary_op_diff():
+    var_name = "var1"
+    op = "diff"
+    expected = "C(var1, Diff)"
+    result = get_term_unary_op(var_name, op)
+    assert expected == result
+
+
+def test_get_term_unary_op_helmert():
+    var_name = "var1"
+    op = "Helmert"
+    expected = "C(var1, Helmert)"
     result = get_term_unary_op(var_name, op)
     assert expected == result
 
@@ -152,3 +228,47 @@ def test_get_term_binary_op_div():
 def test_get_term_binary_op_invalid():
     with pytest.raises(FormulaInvalidOperator):
         get_term_binary_op("", "invalid_op", 0)
+
+
+def test_generate_formula_from_variable_list_only_y(args):
+    args.y = ["a", "b", "c"]
+    expected_formula = "a+b+c-1"
+    formula = generate_formula_from_variable_lists(args)
+    assert formula == expected_formula
+
+
+def test_generate_formula_from_variable_list_xy_with_intercept(args):
+    args.y = ["y"]
+    args.x = ["a", "b", "c"]
+    args.intercept = True
+    expected_formula = "y~a+b+c"
+    formula = generate_formula_from_variable_lists(args)
+    assert formula == expected_formula
+
+
+def test_generate_formula_from_variable_list_xy_without_intercept(args):
+    args.y = ["y"]
+    args.x = ["a", "b", "c"]
+    args.intercept = False
+    expected_formula = "y~a+b+c-1"
+    formula = generate_formula_from_variable_lists(args)
+    assert formula == expected_formula
+
+
+def test_insert_explicit_coding_for_categorical_vars_no_coding(args):
+    args.var_names = ["a", "b", "c"]
+    formula = "a+b+c"
+    is_categorical = {"a": False, "b": False, "c": True}
+    expected_formula = "a+b+C(c, Treatment)"
+    formula = insert_explicit_coding_for_categorical_vars(formula, args, is_categorical)
+    assert formula == expected_formula
+
+
+def test_insert_explicit_coding_for_categorical_vars_with_coding(args):
+    args.var_names = ["a", "b", "c"]
+    args.coding = "Helmert"
+    formula = "a+b+c"
+    is_categorical = {"a": False, "b": False, "c": True}
+    expected_formula = "a+b+C(c, Helmert)"
+    formula = insert_explicit_coding_for_categorical_vars(formula, args, is_categorical)
+    assert formula == expected_formula